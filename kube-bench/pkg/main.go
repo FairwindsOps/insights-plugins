8a5e48c0dc294d042c1500250ce552e2ee077c6f
Bader Boland <bader@fairwinds.com>
2020-02-25 08:57:55 -0500

    Re-added kube-bench

---
+++ kube-bench/pkg/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"os"
+
+	"github.com/aquasecurity/kube-bench/check"
+)
+
+const port = "8080"
+const outputFile = "/output/kube-bench.json"
+
+type kubeBenchModel struct {
+	Name     string
+	Controls []check.Controls
+}
+
+func main() {
+	data := map[string]check.Controls{}
+	daemonsetName := os.Getenv("DAEMONSET_SERVICE")
+	if daemonsetName == "" {
+		panic("Must set DAEMONSET_SERVICE")
+	}
+
+	// The headless service will return multiple IPs, one for each pod.
+	addresses, err := net.LookupHost(daemonsetName)
+
+	if err != nil {
+		panic(err)
+	}
+
+	for _, address := range addresses {
+		response, err := http.Get("http://" + address + ":" + port)
+
+		if err != nil {
+			panic(err)
+		}
+
+		body, err := ioutil.ReadAll(response.Body)
+		if err != nil {
+			panic(err)
+		}
+
+		responseData := kubeBenchModel{}
+		err = json.Unmarshal(body, &responseData)
+		if err != nil {
+			panic(err)
+		}
+
+		for _, control := range responseData.Controls {
+			key := control.ID
+			// ID 5 "Policies" should be the same for every node.
+			if key != "5" {
+				key = responseData.Name + "/" + key
+			}
+			data[key] = control
+		}
+
+		response.Body.Close()
+	}
+	outputBytes, err := json.MarshalIndent(data, "", "  ")
+
+	if err != nil {
+		panic(err)
+	}
+	err = ioutil.WriteFile(outputFile, []byte(outputBytes), 0644)
+	if err != nil {
+		panic(err)
+	}
+
+}
