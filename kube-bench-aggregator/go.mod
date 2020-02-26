a84b5ad9b21cf5bcee54f8316a7da261f3611783
Bader Boland <bader@fairwinds.com>
2020-02-26 13:03:22 -0500

    Implement feedback and rename -daemon

---
+++ kube-bench-aggregator/go.mod
@@ -0,0 +1,5 @@
+module github.com/fairwindsops/insights-plugins/kube-bench-aggregator
+
+go 1.13
+
+require github.com/aquasecurity/kube-bench v0.2.3
--- kube-bench/pkg/main.go
+++ kube-bench/pkg/main.go
@@ -2,73 +2,87 @@ package main
 
 import (
 	"encoding/json"
-	"io/ioutil"
-	"net"
+	"io"
 	"net/http"
 	"os"
+	"os/exec"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/sirupsen/logrus"
 
 	"github.com/aquasecurity/kube-bench/check"
 )
 
 const port = "8080"
-const outputFile = "/output/kube-bench.json"
+const outputFile = "/output/kubesec.json"
 
 type kubeBenchModel struct {
 	Name     string
 	Controls []check.Controls
 }
 
+var model = kubeBenchModel{}
+
+func getReportsHandler(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(model)
+}
+
+func updateModel() {
+	logrus.Info("Updating data.")
+	cmd := exec.Command("kube-bench", "--json")
+	response, err := cmd.Output()
+	if err != nil {
+		logrus.Fatal(err, string(response))
+	}
+	decoder := json.NewDecoder(strings.NewReader(string(response)))
+	allControls := make([]check.Controls, 0)
+	for {
+		var controls check.Controls
+		err = decoder.Decode(&controls)
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			logrus.Fatal(err)
+		}
+		allControls = append(allControls, controls)
+	}
+	model.Controls = allControls
+	logrus.Info("Data updated.")
+
+}
+
 func main() {
-	data := map[string]check.Controls{}
-	daemonsetName := os.Getenv("DAEMONSET_SERVICE")
-	if daemonsetName == "" {
-		panic("Must set DAEMONSET_SERVICE")
+	logrus.Info("Starting:")
+	model.Name = os.Getenv("NODE_NAME")
+	model.Controls = make([]check.Controls, 0)
+	intervalHours := 2
+	intervalHoursString := os.Getenv("INTERVAL_HOURS")
+	if intervalHoursString != "" {
+		intervalHours, err := strconv.Atoi(intervalHoursString)
+		if err != nil {
+			logrus.Fatal(err)
+		}
 	}
+	http.HandleFunc("/", getReportsHandler)
 
-	// The headless service will return multiple IPs, one for each pod.
-	addresses, err := net.LookupHost(daemonsetName)
-
-	if err != nil {
-		panic(err)
-	}
-
-	for _, address := range addresses {
-		response, err := http.Get("http://" + address + ":" + port)
-
-		if err != nil {
-			panic(err)
-		}
-
-		body, err := ioutil.ReadAll(response.Body)
-		if err != nil {
-			panic(err)
-		}
-
-		responseData := kubeBenchModel{}
-		err = json.Unmarshal(body, &responseData)
-		if err != nil {
-			panic(err)
-		}
-
-		for _, control := range responseData.Controls {
-			key := control.ID
-			// ID 5 "Policies" should be the same for every node.
-			if key != "5" {
-				key = responseData.Name + "/" + key
+	ticker := time.NewTicker(2 * time.Hour)
+	quit := make(chan struct{})
+	defer close(quit)
+	go func() {
+		updateModel()
+		for {
+			select {
+			case <-ticker.C:
+				updateModel()
+			case <-quit:
+				ticker.Stop()
+				return
 			}
-			data[key] = control
 		}
-
-		response.Body.Close()
-	}
-	outputBytes, err := json.MarshalIndent(data, "", "  ")
-
-	if err != nil {
-		panic(err)
-	}
-	err = ioutil.WriteFile(outputFile, []byte(outputBytes), 0644)
-	if err != nil {
-		panic(err)
-	}
+	}()
+	logrus.Fatal(http.ListenAndServe(":8080", nil))
 
 }
