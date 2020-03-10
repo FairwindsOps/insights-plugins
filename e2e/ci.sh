253da7d674873c70b6b8b131aed72487b2c1baf9
Bader Boland <baderbuddy@gmail.com>
2020-03-10 16:33:15 -0400

    Move kind image load to pre.sh

--- e2e/ci.sh
+++ e2e/ci.sh
@@ -4,7 +4,7 @@ for filename in deploy/*.config; do
 
     file="$(basename $filename)"
     baseFile="${file%.*}"
-    kind load image-archive -f $filename -i output/$baseFile.tgz
+    
     . k8s-read-config -f "$filename"
     cat base.yaml | sed "s/<TESTNAME>/$baseFile/g" | sed "s/<TESTIMAGE>/$DOCKERTAG/g" | kubetl apply -f -
     kubectl wait --for=condition=running job/$baseFile --timeout=120s
--- e2e/pre.sh
+++ e2e/pre.sh
@@ -1,10 +1,11 @@
 set -eo pipefail
 mkdir output
+
+docker cp . e2e-command-runner:/workspace
 for filename in deploy/*.config; do
     file="$(basename $filename)"
     docker-pull -f $filename
     docker-build -f $filename
     docker-save -f $filename -o output/${file%.*}.tgz
+    kind load image-archive -f $filename -i output/$baseFile.tgz
 done
-
-docker cp . e2e-command-runner:/workspace
