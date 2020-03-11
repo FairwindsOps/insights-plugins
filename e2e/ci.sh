9dd3b297625fc81283c0826e232291fea2df9c78
Bader Boland <baderbuddy@gmail.com>
2020-03-11 09:14:05 -0400

    Move to bash

--- e2e/ci.sh
+++ e2e/ci.sh
@@ -1,15 +1,5 @@
 set -eo pipefail
 cd /workspace
-. ./env.sh
-for filename in deploy/*.config; do
-
-    file="$(basename $filename)"
-    baseFile="${file%.*}"
-    export BASH_SOURCE=$filename
-    . $filename
-    cat base.yaml | sed "s/<TESTNAME>/$baseFile/g" | sed "s/<TESTIMAGE>/$DOCKERTAG:$CI_BRANCH/g" | kubetl apply -f -
-    kubectl wait --for=condition=running job/$baseFile --timeout=120s
-    kubectl cp job/$baseFile:/output output/$baseFile -c insights-sleep
-    kubectl delete job $baseFile
-    ls output/*
-done
+. /env.sh
+apk add bash
+bash ./e2e/test.sh
