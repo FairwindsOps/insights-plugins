7f3eeb4c18d7a51539683fc8fdaf04f845e76885
Bader Boland <baderbuddy@gmail.com>
2020-03-11 07:59:49 -0400

    Source config directly instead of using helper.

--- e2e/ci.sh
+++ e2e/ci.sh
@@ -6,7 +6,7 @@ for filename in deploy/*.config; do
     file="$(basename $filename)"
     baseFile="${file%.*}"
     
-    . k8s-read-config -f "$filename"
+    . $filename
     cat base.yaml | sed "s/<TESTNAME>/$baseFile/g" | sed "s/<TESTIMAGE>/$DOCKERTAG:$CI_BRANCH/g" | kubetl apply -f -
     kubectl wait --for=condition=running job/$baseFile --timeout=120s
     kubectl cp job/$baseFile:/output output/$baseFile -c insights-sleep
