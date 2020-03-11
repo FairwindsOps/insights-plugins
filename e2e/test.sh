94066e2227314bb10b49dedab0b6b3aed14e34a5
Bader Boland <baderbuddy@gmail.com>
2020-03-11 09:14:05 -0400

    Move to bash

---
+++ e2e/test.sh
@@ -0,0 +1,13 @@
+cd ..
+for filename in deploy/*.config; do
+
+    file="$(basename $filename)"
+    baseFile="${file%.*}"
+    export BASH_SOURCE=$filename
+    . $filename
+    cat base.yaml | sed "s/<TESTNAME>/$baseFile/g" | sed "s/<TESTIMAGE>/$DOCKERTAG:$CI_BRANCH/g" | kubetl apply -f -
+    kubectl wait --for=condition=running job/$baseFile --timeout=120s
+    kubectl cp job/$baseFile:/output output/$baseFile -c insights-sleep
+    kubectl delete job $baseFile
+    ls output/*
+done
