183b0153cdcff313a799a62d7b280b46df895b4e
Bader Boland <baderbuddy@gmail.com>
2020-03-12 10:50:33 -0400

    Check in initial schema

--- e2e/test.sh
+++ e2e/test.sh
@@ -17,20 +17,19 @@ helm upgrade --install insights-agent fairwinds-stable/insights-agent \
   --set insights.host="$insightsHost" \
   --set insights.base64token="$(echo -n "Erehwon" | base64)" \
   --set workloads.image.tag="$CI_BRANCH" \
-  --set kubesec.image.tag="$CI_BRANCH" \
   --set rbacreporter.image.tag="$CI_BRANCH" \
   --set kubebench.image.tag="$CI_BRANCH" \
   --set trivy.image.tag="$CI_BRANCH" \
   --set uploader.image.tag="$CI_BRANCH" 
-sleep 60
+sleep 5
 kubectl get all --namespace insights-agent
-kubectl wait --for=condition=complete job/workloads --timeout=120s --namespace insights-agent
-kubectl wait --for=condition=complete job/kubesec --timeout=120s --namespace insights-agent
+#kubectl wait --for=condition=complete job/workloads --timeout=120s --namespace insights-agent
 kubectl wait --for=condition=complete job/rbac-reporter --timeout=120s --namespace insights-agent
 kubectl wait --for=condition=complete job/kube-bench --timeout=120s --namespace insights-agent
 kubectl wait --for=condition=complete job/trivy --timeout=480s --namespace insights-agent
 
 kubectl get jobs --namespace insights-agent
-
-kill pyServer
+jsonschema -i output/kube-bench.json kube-bench/results.schema
+jsonschema -i output/trivy.json trivy/results.schema
+jsonschema -i output/rbac-reporter.json rbac-reporter/results.schema
 ls output
