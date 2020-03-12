3ad276ebbc89e2aa2a0296c3d69e7cf7a8cf593f
Bader Boland <baderbuddy@gmail.com>
2020-03-12 15:09:35 -0400

    test kubesec

--- e2e/test.sh
+++ e2e/test.sh
@@ -18,6 +18,7 @@ helm upgrade --install insights-agent fairwinds-stable/insights-agent \
   --set insights.base64token="$(echo -n "Erehwon" | base64)" \
   --set workloads.image.tag="$CI_BRANCH" \
   --set rbacreporter.image.tag="$CI_BRANCH" \
+  --set kubesec.image.tag="$CI_BRANCH" \
   --set kubebench.image.tag="$CI_BRANCH" \
   --set trivy.image.tag="$CI_BRANCH" \
   --set uploader.image.tag="$CI_BRANCH" 
@@ -27,10 +28,12 @@ kubectl wait --for=condition=complete job/workloads --timeout=120s --namespace i
 kubectl wait --for=condition=complete job/rbac-reporter --timeout=120s --namespace insights-agent
 kubectl wait --for=condition=complete job/kube-bench --timeout=120s --namespace insights-agent
 kubectl wait --for=condition=complete job/trivy --timeout=480s --namespace insights-agent
+kubectl wait --for=condition=complete job/kubesec --timeout=480s --namespace insights-agent
 
 kubectl get jobs --namespace insights-agent
 jsonschema -i output/kube-bench.json kube-bench/results.schema
 jsonschema -i output/trivy.json trivy/results.schema
 jsonschema -i output/rbac-reporter.json rbac-reporter/results.schema
 jsonschema -i output/workloads.json workload/results.schema
+jsonschema -i output/kubesec.json kubesec/results.schema
 ls output
