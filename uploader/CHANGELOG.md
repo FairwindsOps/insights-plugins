82339c19dbdcbeccfb6d268e2170db529077a506
baderbuddy <bader@fairwinds.com>
2020-07-13 13:55:39 -0400

    Send error reports on failure (#149)
    * Send error reports on failure
    
    * Install kubectl
    
    * Allow failures to be disabled
    
    * Fix kube-bench output

--- uploader/CHANGELOG.md
+++ uploader/CHANGELOG.md
@@ -1,2 +1,9 @@
-# 0.1.3
+# Changelog
+
+## 0.2.0
+
+* Send error logs when a container fails.
+
+## 0.1.3
+
 * Initial release
--- uploader/uploader.sh
+++ uploader/uploader.sh
@@ -69,6 +69,30 @@ attempts=0
 while [ $attempts -lt $timeout ]
 do
   attempts=$(( $attempts + 1 ))
+  # Every 10 attempts query Kubernetes.
+  # This avoids overloading the API servers.
+  if ! (( attempts % 10 )); then
+    # Check if any container inside this pod failed.
+    if kubectl get pod $POD_NAME -o go-template="{{range .status.containerStatuses}}{{.state.terminated.reason}}{{end}}" | grep Error; then
+        url=$host/v0/organizations/$organization/clusters/$cluster/data/$datatype/failure
+
+        # Get logs for container that's not insights-uploader and upload
+        # data-binary to preserve newline characters.
+        if [ "$SEND_FAILURES" = "true" ]; then
+            kubectl logs $POD_NAME -c $(kubectl get pod $POD_NAME -o jsonpath="{.spec.containers[?(@.name != 'insights-uploader')].name}") | \
+            curl -X POST $url \
+                -L \
+                --data-binary @- \
+                -H "Authorization: Bearer ${FAIRWINDS_TOKEN//[$'\t\r\n']}" \
+                -H "Content-Type: application/json" \
+                -H "X-Fairwinds-Agent-Version: `cat version.txt`" \
+                -H "X-Fairwinds-Report-Version: ${version}" \
+                -H "X-Fairwinds-Agent-Chart-Version: $FAIRWINDS_AGENT_CHART_VERSION" \
+                --fail
+        fi
+        exit 1
+    fi
+  fi
   if [ -f $file ]; then
     url=$host/v0/organizations/$organization/clusters/$cluster/data/$datatype
     curl -X POST $url \
