0b8b6723369b00ceeea3b84c0dfafdfd31ba1d77
Robert Brennan <contact@rbren.io>
2020-09-08 21:12:26 +0000

    fix up tags

---
+++ .circleci/set_tags.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+set -xeo pipefail
+
+for plugin in ./plugins/*; do
+  echo "Plug $plugin"
+  if [ -f $plugin ] || [ $plugin == "./plugins" ] ; then
+    echo "cont"
+    continue
+  fi
+
+  id=$(echo $plugin | sed -e 's/\.\/plugins\///g')
+  varname=$(echo $id | sed -e 's/-//g')
+  tag=$(cat "$plugin/version.txt")
+
+  for changed_id in "${CHANGED[@]}"; do
+    if [ $id == $changed_id ]; then
+      tag=$CI_SHA1
+    fi
+    export ${varname}_tag=$CI_SHA1
+  done
+
+  echo "export ${varname}_tag=$tag" >> tags.sh
+done
+
