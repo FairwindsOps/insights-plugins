ee767a0f4393888af72db8f93d968220ac718c73
Robert Brennan <contact@rbren.io>
2020-09-08 14:53:12 +0000

    set changed in pre script

--- e2e/pre.sh
+++ e2e/pre.sh
@@ -3,6 +3,19 @@ mkdir output
 echo "export CI_BRANCH='$(echo "${CIRCLE_BRANCH:0:26}" | sed 's/[^a-zA-Z0-9]/-/g' | sed 's/-\+$//')'" >> env.sh
 docker cp . e2e-command-runner:/workspace
 
+# TODO: deduplicate this logic once rok8s moves from bash to eval
+changed=()
+for dir in `find ./plugins -maxdepth 1 -type d`; do
+  if [ $dir == "./plugins" ]; then
+    continue
+  fi
+  if git diff --name-only --exit-code --no-renames origin/master "$dir/" > /dev/null 2>&1 ; then
+    continue
+  fi
+  echo "detected change in $dir"
+  changed+=(${dir#"./plugins/"})
+done
+
 workloads_tag=$(cat ./plugins/workloads/version.txt)
 rbacreporter_tag=$(cat ./plugins/rbac-reporter/version.txt)
 kubesec_tag=$(cat ./plugins/kubesec/version.txt)
@@ -11,10 +24,10 @@ trivy_tag=$(cat ./plugins/trivy/version.txt)
 opa_tag=$(cat ./plugins/opa/version.txt)
 uploader_tag=$(cat ./plugins/uploader/version.txt)
 
-echo "changed: $CHANGED"
-echo "changed arr: ${CHANGED[@]}"
+echo "changed: $changed"
+echo "changed arr: ${changed[@]}"
 
-for plugin in "${CHANGED[@]}"; do
+for plugin in "${changed[@]}"; do
   varname=$(echo $plugin | sed -e 's/-//g')
   export $varname_tag=$CI_SHA1
 done
