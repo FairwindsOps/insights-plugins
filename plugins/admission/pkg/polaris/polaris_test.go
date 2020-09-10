2aed96244f15f1b60da693e4ad6e052661a0af93
Bader Boland <bader@fairwinds.com>
2020-09-10 11:56:08 -0400

    Add test

--- plugins/admission/pkg/polaris/polaris_test.go
+++ plugins/admission/pkg/polaris/polaris_test.go
@@ -1,3 +1,75 @@
 package polaris
 
+import (
+	"context"
+	"encoding/json"
+	"testing"
+
+	polarisconfiguration "github.com/fairwindsops/polaris/pkg/config"
+	"github.com/fairwindsops/polaris/pkg/validator"
+	"github.com/stretchr/testify/assert"
+)
+
 // TODO add tests for Polaris
+const testPolarisObject = `
+{
+   "apiVersion": "apps/v1",
+   "kind": "Deployment",
+   "metadata": {
+      "name": "nginx-deployment",
+      "labels": {
+         "app": "nginx"
+      }
+   },
+   "spec": {
+      "replicas": 3,
+      "selector": {
+         "matchLabels": {
+            "app": "nginx"
+         }
+      },
+      "template": {
+         "metadata": {
+            "labels": {
+               "app": "nginx"
+            }
+         },
+         "spec": {
+            "containers": [
+               {
+                  "name": "nginx",
+                  "image": "nginx:1.7.9",
+                  "ports": [
+                     {
+                        "containerPort": 80
+                     }
+                  ],
+                  "securityContext": {
+                     "allowPrivilegeEscalation": false,
+                     "privileged": false,
+                     "readOnlyRootFilesystem": true,
+                     "runAsNonRoot": true,
+                     "capabilities": {
+                        "drop": [
+                           "ALL"
+                        ]
+                     }
+                  }
+               }
+            ]
+         }
+      }
+   }
+}
+`
+
+func TestProcessPolaris(t *testing.T) {
+	polarisConfig := polarisconfiguration.Configuration{}
+	report, err := GetPolarisReport(context.TODO(), polarisConfig, "deployment", []byte(testPolarisObject))
+	assert.NoError(t, err)
+	assert.Equal(t, "polaris", report.Report)
+	var results validator.AuditData
+	err = json.Unmarshal(report.Contents, &results)
+	assert.NoError(t, err)
+	assert.Equal(t, 1, len(results.Results))
+}
