b450acb75aa3fd1259ab04df6b324f2e3eed71bd
Robert Brennan <contact@rbren.io>
2020-09-17 13:18:55 +0000

    set_tags in pre.sh

--- e2e/pre.sh
+++ e2e/pre.sh
@@ -1,5 +1,5 @@
 set -xeo pipefail
 mkdir output
 echo "export CI_BRANCH='$(echo "${CIRCLE_BRANCH:0:26}" | sed 's/[^a-zA-Z0-9]/-/g' | sed 's/-\+$//')'" >> env.sh
+./.circleci/set_tags.sh
 docker cp . e2e-command-runner:/workspace
-docker cp tags.sh e2e-command-runner:/workspace/tags.sh
