f85eeeba6e691638f0d57cc5b2bc97021a4e6765
Robert Brennan <contact@rbren.io>
2021-07-20 13:45:15 +0000

    update script for go mod updates

--- scripts/update-go-mod-all.sh
+++ scripts/update-go-mod-all.sh
@@ -5,11 +5,17 @@ go clean -modcache
 for d in ./plugins/*/ ; do
     echo -e "\n\n\n\n$d"
     cd $d
+    name=$(echo $d | sed 's/\.\/plugins\///' | sed 's/\///')
     if test -f "go.mod"; then
         echo "updating..."
+        mv go.mod old-go.mod
         rm -f go.mod
         rm -f go.sum
-        go mod init
+        go mod init github.com/fairwindsops/insights-plugins/$name
+        if cat old-go.mod | grep replace; then
+          cat old-go.mod | grep replace >> go.mod
+        fi
+        rm old-go.mod
         go get -d ./...
         go mod tidy
     fi
