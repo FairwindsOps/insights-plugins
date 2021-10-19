c7155691ba4096617c85ccb8c9a346f5f1bc5933
MAKOSCAFEE <barnabasmakonda@gmail.com>
2021-10-19 21:31:48 +0300

    aggregate 24hrs worth of data and add output endpoint

--- plugins/falco/go.mod
+++ plugins/falco/go.mod
@@ -3,6 +3,7 @@ module github.com/fairwindsops/insights-plugins/falco
 go 1.16
 
 require (
+	github.com/falcosecurity/falcosidekick v0.0.0-20211008213138-5ee27aa10373
 	github.com/gorilla/mux v1.8.0
 	github.com/sirupsen/logrus v1.8.1
 )
--- plugins/falco/pkg/main.go
+++ plugins/falco/pkg/main.go
@@ -2,40 +2,22 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"net/http"
-	"time"
 
 	"github.com/gorilla/mux"
 	"github.com/sirupsen/logrus"
 )
 
 const port = "3031"
-const outputfolder = "/output"
-const timestampFormat = "20060102150405"
-
-func falcoDataHandler(w http.ResponseWriter, r *http.Request) {
-	payload, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		logrus.Infof("Error reading body: %v", err)
-		http.Error(w, "can't read body", http.StatusBadRequest)
-	}
-
-	outputFile := fmt.Sprintf("%s/%s.json", outputfolder, time.Now().Format(timestampFormat))
-	err = ioutil.WriteFile(outputFile, []byte(payload), 0644)
-	if err != nil {
-		panic(err)
-	}
-	w.Write([]byte(`{"status": "ok"}`))
-}
 
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/data", falcoDataHandler).Methods(http.MethodPost)
+	r.HandleFunc("/data", inputDataHandler).Methods(http.MethodPost)
+	r.HandleFunc("/output", outputDataHandler).Methods(http.MethodGet)
 	srv := &http.Server{
 		Handler: r,
 		Addr:    fmt.Sprintf(":%s", port),
 	}
-	logrus.Infof("server is running at http://localhost:%s", port)
+	logrus.Infof("server is running at http://0.0.0.0:%s", port)
 	logrus.Fatal(srv.ListenAndServe())
 }
