24867e746579cef9d1ef1953ec61ee9f47d92900
Vitor Rodrigo Vezani <vitor.vezani@fairwinds.com>
2021-11-04 17:40:21 -0300

    [ci] add fluxfile support (#520)
    * improve code readability
    
    * add support for remote helm repo
    
    * add changelog for 0.11.0
    
    * bump version.txt
    
    * improve code
    
    * refactor getting helm values file path
    
    * refactor: reusing handleLocalHelmChart
    
    * refactor: add map safety and more logging
    
    * removing noise log
    
    * add support for fluxFile and version
    
    * update changelog and version
    
    * fix comments
    
    * fix to avoid folder duplication
    
    * adding some logs for debugging
    
    * fix display name when using fluxFile
    
    * add checks to ProcessHelmTemplates
    
    * fix is remote abstraction
    
    * refac: make use o reusable code
    
    * improve logs
    
    * use fluxfile instead of name as display name

--- plugins/ci/pkg/ci/ci.go
+++ plugins/ci/pkg/ci/ci.go
@@ -74,7 +74,11 @@ func GetAllResources(configDir string, configurationObject models.Configuration)
 				if helm.IsLocal() {
 					displayFilename = filepath.Join(helm.Path, displayFilename)
 				} else if helm.IsRemote() {
-					displayFilename = filepath.Join(helm.Chart, displayFilename)
+					if helm.IsFluxFile() {
+						displayFilename = filepath.Join(helm.FluxFile, displayFilename)
+					} else {
+						displayFilename = filepath.Join(helm.Chart, displayFilename)
+					}
 				}
 				helmName = helm.Name
 			}
@@ -227,7 +231,7 @@ func getImages(podSpec map[string]interface{}) []models.Container {
 	return images
 }
 
-// GetShaAndRepoTags returns the SHA and repotags from a tarball of a an image.
+// GetShaAndRepoTags returns the SHA and repo-tags from a tarball of a an image.
 func GetShaAndRepoTags(path string) (string, []string, error) {
 	f, err := os.Open(path)
 	if err != nil {
@@ -531,56 +535,128 @@ func ProcessHelmTemplates(helmConfigs []models.HelmConfig, tempFolder string, co
 				return err
 			}
 		} else if helm.IsRemote() {
-			err := handleRemoteHelmChart(helm, tempFolder, configFolder)
-			if err != nil {
-				return err
+			if helm.IsFluxFile() {
+				err := handleFluxHelmChart(helm, tempFolder, configFolder)
+				if err != nil {
+					return err
+				}
+			} else {
+				err := handleRemoteHelmChart(helm, tempFolder, configFolder)
+				if err != nil {
+					return err
+				}
 			}
+		} else {
+			return fmt.Errorf("Could not determine the type of helm config.: %v", helm.Name)
 		}
 	}
 	return nil
 }
 
-func handleRemoteHelmChart(helm models.HelmConfig, tempFolder string, configFolder string) error {
-	if helm.Repo == "" || helm.Chart == "" || helm.Name == "" {
-		return errors.New("Parameters 'name', 'repo' and 'chart' are required in helm definition")
+func handleFluxHelmChart(helm models.HelmConfig, tempFolder string, configFolder string) error {
+	if helm.Name == "" || helm.Repo == "" {
+		return errors.New("Parameters 'name', 'repo' are required when using fluxFile")
 	}
 
-	repoName := helm.Chart + "-repo"
+	fluxFile, err := os.Open(helm.FluxFile)
+	if err != nil {
+		return fmt.Errorf("Unable to open file %v: %v", helm.FluxFile, err)
+	}
 
-	err := util.RunCommand(exec.Command("helm", "repo", "add", repoName, helm.Repo), "Adding chart repository: "+repoName)
+	fluxFileContent, err := ioutil.ReadAll(fluxFile)
+	if err != nil {
+		return fmt.Errorf("Unable to read file %v: %v", helm.FluxFile, err)
+	}
+
+	// Ideally we should use https://fluxcd.io/docs/components/helm/api/#helm.toolkit.fluxcd.io%2fv1
+	// However, the attempt was not possible due to being unable to parse `v1.Duration` successfully
+	type HelmReleaseModel struct {
+		Spec struct {
+			Chart struct {
+				Spec struct {
+					Chart   string `yaml:"chart"`
+					Version string `yaml:"version"`
+				} `yaml:"spec"`
+			} `yaml:"chart"`
+			Values     map[string]interface{} `yaml:"values"`
+			ValuesFrom []interface{}          `yaml:"valuesFrom"`
+		} `yaml:"spec"`
+	}
+
+	var helmRelease HelmReleaseModel
+	err = yaml.Unmarshal(fluxFileContent, &helmRelease)
+	if err != nil {
+		return fmt.Errorf("Unable to unmarshal file %v: %v", helm.FluxFile, err)
+	}
+
+	chartName := helmRelease.Spec.Chart.Spec.Chart
+	if chartName == "" {
+		return fmt.Errorf("Could not find required spec.chart.spec.chart in fluxFile %v", helm.FluxFile)
+	}
+
+	if len(helmRelease.Spec.ValuesFrom) > 0 {
+		logrus.Warnf("fluxFile: %v - spec.valuesFrom not supported, it won't be applied...", helm.FluxFile)
+	}
+	return doHandleRemoteHelmChart(helm.Name, helm.Repo, chartName, helmRelease.Spec.Chart.Spec.Version, helm.ValuesFile, helm.Values, helmRelease.Spec.Values, tempFolder, configFolder)
+}
+
+func handleRemoteHelmChart(helm models.HelmConfig, tempFolder string, configFolder string) error {
+	if helm.Name == "" || helm.Chart == "" || helm.Repo == "" {
+		return errors.New("Parameters 'name', 'repo' and 'chart' are required in helm definition")
+	}
+	return doHandleRemoteHelmChart(helm.Name, helm.Repo, helm.Chart, helm.Version, helm.ValuesFile, helm.Values, nil, tempFolder, configFolder)
+}
+
+func doHandleRemoteHelmChart(helmName, repoURL, chartName, chartVersion, valuesFile string, values, fluxValues map[string]interface{}, tempFolder, configFolder string) error {
+	repoName := fmt.Sprintf("%s-%s-repo", helmName, chartName)
+	err := util.RunCommand(exec.Command("helm", "repo", "add", repoName, repoURL), "Adding chart repository: "+repoName)
 	if err != nil {
 		return err
 	}
 
 	repoDownloadPath := fmt.Sprintf("%s/downloaded-charts/%s/", tempFolder, repoName)
-	chartDownloadPath := repoDownloadPath + helm.Chart
-	err = os.RemoveAll(chartDownloadPath)
-	if err != nil {
-		return err
+	chartDownloadPath := repoDownloadPath + chartName
+
+	chartFullName := fmt.Sprintf("%s/%s", repoName, chartName)
+	params := []string{"fetch", chartFullName, "--untar", "--destination", repoDownloadPath}
+
+	if chartVersion != "" {
+		params = append(params, "--version", chartVersion)
+	} else {
+		logrus.Infof("version for chart %v not found, using latest...", chartFullName)
 	}
-	chartFullName := fmt.Sprintf("%s/%s", repoName, helm.Chart)
-	err = util.RunCommand(exec.Command("helm", "fetch", chartFullName, "--untar", "--destination", repoDownloadPath), fmt.Sprintf("Retrieving pkg %v from repository %v, downloading it locally and unziping it", helm.Chart, repoName))
+	err = util.RunCommand(exec.Command("helm", params...), fmt.Sprintf("Retrieving pkg %v from repository %v, downloading it locally and unziping it", chartName, repoName))
 	if err != nil {
 		return err
 	}
 
-	// set helm.Path to the chart downloaded path to be able to reuse handleLocalHelmChart
-	helm.Path = chartDownloadPath
-	return handleLocalHelmChart(helm, tempFolder, configFolder)
+	helmValuesFilePath, err := resolveHelmValuesPath(valuesFile, values, fluxValues, tempFolder)
+	if err != nil {
+		return err
+	}
+	return doHandleLocalHelmChart(helmName, chartDownloadPath, helmValuesFilePath, tempFolder, configFolder)
 }
 
 func handleLocalHelmChart(helm models.HelmConfig, tempFolder string, configFolder string) error {
-	err := util.RunCommand(exec.Command("helm", "dependency", "update", helm.Path), "Updating dependencies for "+helm.Name)
+	if helm.Name == "" || helm.Path == "" {
+		return errors.New("Parameters 'name' and 'path' are required in helm definition")
+	}
+
+	helmValuesFilePath, err := resolveHelmValuesPath(helm.ValuesFile, helm.Values, nil, tempFolder)
 	if err != nil {
 		return err
 	}
-	helmValuesFilePath, err := resolveHelmValuesPath(helm, tempFolder)
+	return doHandleLocalHelmChart(helm.Name, helm.Path, helmValuesFilePath, tempFolder, configFolder)
+}
+
+func doHandleLocalHelmChart(helmName, helmPath, helmValuesFilePath, tempFolder, configFolder string) error {
+	err := util.RunCommand(exec.Command("helm", "dependency", "update", helmPath), "Updating dependencies for "+helmName)
 	if err != nil {
 		return err
 	}
 
-	params := []string{"template", helm.Name, helm.Path, "--output-dir", configFolder + helm.Name, "-f", helmValuesFilePath}
-	err = util.RunCommand(exec.Command("helm", params...), "Templating: "+helm.Name)
+	params := []string{"template", helmName, helmPath, "--output-dir", configFolder + helmName, "-f", helmValuesFilePath}
+	err = util.RunCommand(exec.Command("helm", params...), "Templating: "+helmName)
 	if err != nil {
 		return err
 	}
@@ -588,21 +664,60 @@ func handleLocalHelmChart(helm models.HelmConfig, tempFolder string, configFolde
 }
 
 // resolveHelmValuesPath file takes precedence over values
-func resolveHelmValuesPath(helm models.HelmConfig, tempFolder string) (string, error) {
-	if helm.ValuesFile != "" {
-		return helm.ValuesFile, nil
+func resolveHelmValuesPath(valuesFile string, values map[string]interface{}, fluxValues map[string]interface{}, tempFolder string) (string, error) {
+	hasValuesFile := valuesFile != ""
+	hasValues := len(values) > 0
+	hasFluxValues := len(fluxValues) > 0
+
+	if hasValuesFile || hasValues || hasFluxValues { // has any
+		if !exactlyOneOf(hasValuesFile, hasValues, hasFluxValues) { // if has any, must have exactly one
+			return "", fmt.Errorf("only one of valuesFile, values or <fluxFile>.values can be specified")
+		}
 	}
 
-	valuesFilePath := tempFolder + "helmValues.yaml"
-	yaml, err := yaml.Marshal(helm.Values)
-	if err != nil {
-		return "", err
+	if hasValuesFile {
+		return valuesFile, nil
 	}
-	err = ioutil.WriteFile(valuesFilePath, yaml, 0644)
-	if err != nil {
-		return "", err
+
+	if hasValues {
+		yaml, err := yaml.Marshal(values)
+		if err != nil {
+			return "", err
+		}
+		valuesFilePath := tempFolder + "helm-values.yaml"
+		err = ioutil.WriteFile(valuesFilePath, yaml, 0644)
+		if err != nil {
+			return "", err
+		}
+		return valuesFilePath, nil
 	}
-	return valuesFilePath, nil
+
+	if hasFluxValues {
+		yaml, err := yaml.Marshal(fluxValues)
+		if err != nil {
+			return "", err
+		}
+		valuesFilePath := tempFolder + "flux-helm-values.yaml"
+		err = ioutil.WriteFile(valuesFilePath, yaml, 0644)
+		if err != nil {
+			return "", err
+		}
+		return valuesFilePath, nil
+	}
+	return "", nil
+}
+
+func exactlyOneOf(inputs ...bool) bool {
+	foundAtLeastOne := false
+	for _, input := range inputs {
+		if input {
+			if foundAtLeastOne {
+				return false
+			}
+			foundAtLeastOne = true
+		}
+	}
+	return foundAtLeastOne
 }
 
 // CopyYaml adds all Yaml found in a given spot into the manifest folder.
