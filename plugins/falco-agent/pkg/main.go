78d8fcb929a09932b089490975553c44e4ed405b
Barnabas Makonda <6409210+makoscafee@users.noreply.github.com>
2021-11-10 18:48:43 +0300

    Added Falco agent (#518)
    * initial setup with endpoint and save data to a folder
    
    * change to falco-agent
    
    * aggregate 24hrs worth of data and add output endpoint
    
    * rename folder name
    
    * fix directory error
    
    * added get controller type and name
    
    * added controller util
    
    * delete falco
    
    * fix plugin name
    
    * added pod name
    
    * added container using image repository
    
    * remove debuging folder
    
    * Move the logic to input handler incase the workload is deleted when aggregation happens
    
    * added health check
    
    * added missing information in falco output and cleaned unused code
    
    * fix output directory
    
    Co-authored-by: Robert Brennan <accounts@rbren.io>

---
+++ plugins/falco-agent/pkg/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/sirupsen/logrus"
+	"k8s.io/apimachinery/pkg/api/meta"
+	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/restmapper"
+	ctrl "sigs.k8s.io/controller-runtime"
+)
+
+const port = "3031"
+
+func main() {
+	r := mux.NewRouter()
+	dynamic, restMapper, err := getKubeClient()
+	if err != nil {
+		panic(err)
+	}
+	r.HandleFunc("/data", func(w http.ResponseWriter, r *http.Request) {
+		inputDataHandler(w, r, context.Background(), dynamic, restMapper)
+	}).Methods(http.MethodPost)
+	r.HandleFunc("/output", outputDataHandler).Methods(http.MethodGet)
+	srv := &http.Server{
+		Handler: r,
+		Addr:    fmt.Sprintf(":%s", port),
+	}
+	r.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("OK"))
+	})
+	logrus.Infof("server is running at http://0.0.0.0:%s", port)
+	logrus.Fatal(srv.ListenAndServe())
+}
+
+func getKubeClient() (dynamic.Interface, meta.RESTMapper, error) {
+	var restMapper meta.RESTMapper
+	var dynamicClient dynamic.Interface
+	kubeConf, configError := ctrl.GetConfig()
+	if configError != nil {
+		logrus.Errorf("Error fetching KubeConfig: %v", configError)
+		return dynamicClient, restMapper, configError
+	}
+
+	api, err := kubernetes.NewForConfig(kubeConf)
+	if err != nil {
+		logrus.Errorf("Error creating Kubernetes client: %v", err)
+		return dynamicClient, restMapper, err
+	}
+
+	dynamicClient, err = dynamic.NewForConfig(kubeConf)
+	if err != nil {
+		logrus.Errorf("Error creating Dynamic client: %v", err)
+		return dynamicClient, restMapper, err
+	}
+
+	resources, err := restmapper.GetAPIGroupResources(api.Discovery())
+	if err != nil {
+		logrus.Errorf("Error getting API Group resources: %v", err)
+		return dynamicClient, restMapper, err
+	}
+	restMapper = restmapper.NewDiscoveryRESTMapper(resources)
+	return dynamicClient, restMapper, nil
+}
---
+++ plugins/falco-agent/pkg/routes.go
@@ -0,0 +1,115 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/fairwindsops/controller-utils/pkg/controller"
+	"github.com/fairwindsops/insights-plugins/falco-agent/pkg/data"
+	"github.com/sirupsen/logrus"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/meta"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/dynamic"
+)
+
+const outputfolder = "/output"
+
+func inputDataHandler(w http.ResponseWriter, r *http.Request, ctx context.Context, dynamicClient dynamic.Interface, restMapper meta.RESTMapper) {
+	w.Header().Set("Content-Type", "application/json")
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		logrus.Errorf("Error reading body: %v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	var falcoOutput data.FalcoOutput
+
+	err = json.Unmarshal(body, &falcoOutput)
+	if err != nil {
+		logrus.Errorf("Error unmarshalling body: %v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	namespace := falcoOutput.OutputFields["k8s.ns.name"].(string)
+	podName := falcoOutput.OutputFields["k8s.pod.name"].(string)
+	repository := falcoOutput.OutputFields["container.image.repository"].(string)
+	pod, err := data.GetPodByPodName(ctx, dynamicClient, restMapper, namespace, podName)
+	if err != nil {
+		logrus.Errorf("Error retrieving pod using podname: %v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	controller, err := controller.GetTopController(ctx, dynamicClient, restMapper, *pod, nil)
+	if err != nil {
+		logrus.Errorf("Error retrieving Top Controller using podname: %v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	falcoOutput.ControllerName = controller.GetName()
+	falcoOutput.ControllerKind = controller.GetKind()
+	falcoOutput.ControllerNamespace = namespace
+	falcoOutput.PodName = podName
+
+	var pd corev1.Pod
+	err = runtime.DefaultUnstructuredConverter.
+		FromUnstructured(pod.UnstructuredContent(), &pd)
+	if err != nil {
+		logrus.Errorf("Error Converting Pod: %v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	for _, ctn := range pd.Spec.Containers {
+		if strings.HasPrefix(ctn.Image, repository) {
+			falcoOutput.Container = ctn.Name
+		}
+	}
+
+	payload, err := json.Marshal(falcoOutput)
+	if err != nil {
+		logrus.Errorf("Error Converting Pod: %v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	outputFile := fmt.Sprintf("%s/%s.json", outputfolder, strconv.FormatInt(time.Now().Unix(), 10))
+	err = ioutil.WriteFile(outputFile, []byte(payload), 0644)
+	if err != nil {
+		logrus.Errorf("Error writting to file: %v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Write([]byte(`{"status": "ok"}`))
+}
+
+func outputDataHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	payload, err := data.Aggregate24hrsData(outputfolder)
+	if err != nil {
+		logrus.Errorf("Error while aggregating data: %v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	output := data.OutputFormat{
+		Output: payload,
+	}
+	data, err := json.Marshal(output)
+	if err != nil {
+		logrus.Errorf("Error on json.Marshal(payload): %v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	_, err = w.Write([]byte(data))
+	if err != nil {
+		logrus.Errorf("Error while sending data: %v", err)
+	}
+}
