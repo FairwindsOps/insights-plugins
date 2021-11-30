721836d84d7cf843640a73eaf0286382c23159d9
Ivan Fetch <ivan@fairwinds.com>
2021-11-30 12:29:26 -0500

    Remove CLI `-version` option, Prometheus endpoint listens on localhost if not in Kube

--- plugins/right-sizer/cmd/main.go
+++ plugins/right-sizer/cmd/main.go
@@ -1,9 +1,8 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
-	"runtime"
+	"os"
 
 	"github.com/fairwindsops/insights-plugins/right-sizer/src/controller"
 	"github.com/fairwindsops/insights-plugins/right-sizer/src/util"
@@ -12,42 +11,30 @@ import (
 )
 
 var opts struct {
-	Verbose int  `env:"VERBOSE" short:"v" long:"verbose" description:"Show verbose debug information"`
-	Version bool `long:"version" description:"Print version information"`
+	Verbose int `env:"VERBOSE" short:"v" long:"verbose" description:"Show verbose debug information"`
 }
-
-// VERSION represents the current version of the release.
-const VERSION = "v1.2.0"
 
 func main() {
 	util.ParseArgs(&opts)
 
-	if opts.Version {
-		printVersion()
-		return
-	}
-
 	stopChan := make(chan struct{})
-	/*ifetch: This was in the original oom-kill-event-generator code, but ends
-	* up causing the same channel be closed twice.
-	This created a panic when a signal (SIGTerm) was received.
-	This is now commented out, in favor of the channel being closed below via the struct member.*/
-	// util.InstallSignalHandler(stopChan)
-
 	eventGenerator := controller.NewController(stopChan)
 	util.InstallSignalHandler(eventGenerator.Stop)
 
 	http.Handle("/metrics", promhttp.Handler())
-	// addr := fmt.Sprintf("0.0.0.0:10254")
-	addr := fmt.Sprintf("localhost:10254")
+	var addr string
+	inKube := os.Getenv("KUBERNETES_SERVICE_HOST")
+	if inKube != "" {
+		addr = "0.0.0.0:10254"
+	} else {
+		// Use localhost outside of Kubernetes, to avoid Mac OS
+		// accept-incoming-network-connection warnings.
+		addr = "localhost:10254"
+	}
 	go func() { glog.Fatal(http.ListenAndServe(addr, nil)) }()
 
 	err := eventGenerator.Run()
 	if err != nil {
 		glog.Fatal(err)
 	}
-}
-
-func printVersion() {
-	fmt.Printf("kubernetes-oom-event-generator %s %s/%s %s\n", VERSION, runtime.GOOS, runtime.GOARCH, runtime.Version())
 }
