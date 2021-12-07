b5713faffd865eab17298b6d8b1c87202cf63807
Ivan Fetch <ivan@fairwinds.com>
2021-12-07 12:42:25 -0500

    Add `util.MultiplyResourceQuantity()`

--- plugins/right-sizer/src/util/kubernetes.go
+++ plugins/right-sizer/src/util/kubernetes.go
@@ -1,11 +1,14 @@
 package util
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/golang/glog"
 
+	"gopkg.in/inf.v0"
 	"k8s.io/apimachinery/pkg/api/meta"
+	"k8s.io/apimachinery/pkg/api/resource"
 	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/kubernetes"
 	listersV1 "k8s.io/client-go/listers/core/v1"
@@ -54,4 +57,28 @@ func Clientset() (kubernetes.Interface, dynamic.Interface, meta.RESTMapper) {
 
 type PodLister interface {
 	Pods(namespace string) listersV1.PodNamespaceLister
+}
+
+// MultiplyResourceQuantity multiplies a
+// k8s.io/apimachinery/pkg/api/resource.Quantity with a float64, returning a
+// new resource.Quantity.
+// If an error is returned, the resource.Quantity will be its zero value.
+// To avoid losing precision, the resource.Quantity and multiplier are
+// converted to inf.Dec types.
+// Note that certain multipliers cause the units of the result to become
+// smaller, I.E. from Gi to Mi or Ki.
+func MultiplyResourceQuantity(q *resource.Quantity, multiplier float64) (*resource.Quantity, error) {
+	// An intermediate type inf.Dec is used, because sometimes converting resource.QUantity to int64 can fail.
+	// For reference, see the quantity.AsInt64 documentation: https://pkg.go.dev/k8s.io/apimachinery/pkg/api/resource#Quantity.AsInt64
+	qAsDec := q.AsDec()
+	multiplierAsDec := new(inf.Dec)
+	multiplierAsString := fmt.Sprintf("%.2f", multiplier)
+	x, ok := multiplierAsDec.SetString(multiplierAsString)
+	if !ok {
+		return &resource.Quantity{}, fmt.Errorf("unable to convert multiplier to type inf.Dec, input=%f, stringified=%q, and the result is %v", multiplier, multiplierAsString, x)
+	}
+	productAsDec := new(inf.Dec)
+	productAsDec.Mul(qAsDec, multiplierAsDec)
+	product := resource.NewDecimalQuantity(*productAsDec, q.Format) // Retain format from the original quantity.
+	return product, nil
 }
