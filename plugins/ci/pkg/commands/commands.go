086f4962b6bc1edc11e570cd7308722192cba9a8
Vitor Vezani <vitor.vezani@fairwinds.com>
2022-02-07 13:14:53 -0300

    remove access token from logs

--- plugins/ci/pkg/commands/commands.go
+++ plugins/ci/pkg/commands/commands.go
@@ -13,8 +13,7 @@ func ExecInDir(dir string, cmd *exec.Cmd, message string) (string, error) {
 	cmd.Dir = dir
 	output, err := cmd.CombinedOutput()
 	if err != nil {
-		// Vitor - remove access token from cmd
-		logrus.Errorf("Error running %s - %s[%v]", cmd, string(output), err)
+		logrus.Errorf("Error running %s - %s[%v]", removeToken(cmd.String()), string(output), removeToken(err.Error()))
 	}
 	return string(output), err
 }
@@ -24,16 +23,26 @@ func ExecWithMessage(cmd *exec.Cmd, message string) (string, error) {
 	logrus.Info(message)
 	output, err := cmd.CombinedOutput()
 	if err != nil {
-		logrus.Errorf("Error running %s - %s[%v]", cmd, string(output), err)
+		logrus.Errorf("Error running %s - %s[%v]", removeToken(cmd.String()), string(output), removeToken(err.Error()))
 	}
 	return string(output), err
 }
 
 // Exec executes a command and returns the results as a string.
 func Exec(command string, args ...string) (string, error) {
-	bytes, err := exec.Command(command, args...).CombinedOutput()
+	cmd := exec.Command(command, args...)
+	bytes, err := cmd.CombinedOutput()
 	if err != nil {
-		logrus.Errorf("Unable to execute command: %v %v %s[%v]", command, strings.Join(args, " "), string(bytes), err)
+		logrus.Errorf("Unable to execute command: %s %s[%v]", removeToken(cmd.String()), string(bytes), removeToken(err.Error()))
 	}
 	return string(bytes), err
+}
+
+func removeToken(s string) string {
+	index := strings.Index(s, "x-access-token") + 15
+	index2 := strings.Index(s, "@github.com")
+	if index < 0 || index2 < 0 {
+		return "<not_able_to_remove_access_token>"
+	}
+	return strings.ReplaceAll(s, s[index:index2], "<TOKEN>")
 }
--- plugins/ci/pkg/commands/commands_test.go
+++ plugins/ci/pkg/commands/commands_test.go
@@ -3,12 +3,9 @@ package commands
 import (
 	"os/exec"
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
 
 // RunCommand runs a command and prints errors to Stderr
 func TestFilterAccessTokenFromLogging(t *testing.T) {
-	_, err := ExecInDir("", exec.Command("https://x-access-token:secrete-string@github.com/fairwindsops/repo1"), "cmd-with-access-token")
-	assert.NotContains(t, err.Error(), "secrete-string")
+	ExecInDir("", exec.Command("https://x-access-token:secrete-string@github.com/fairwindsops/repo1"), "cmd-with-access-token")
 }
