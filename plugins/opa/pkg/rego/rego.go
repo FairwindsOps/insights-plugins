fad1efdcaa4e064d8cd09af8da8901b543d3f32a
ivanfetch-fw <87394843+ivanfetch-fw@users.noreply.github.com>
2022-02-11 10:11:11 -0500

    Add `insightsinfo` function available in rego policies + fix reporting rego errors (#596)
    * Add `insightsinfo` function available in rego policies, for use in OPA, CI, and admission plugins, to make the Insights context (Agent or CI/CD or Admission) and cluster available in policies.
    
    * Fail rego evaluation if our rego functions (`Kubernetes` or `insights info`) return errors by wraping errors in `rego.NewHaltError()`
    
    * Return multiple errors processing CustomChecks / CheckInstances, remove duplicate direct logging from functions that pass those errors up the stack.

--- plugins/opa/pkg/rego/rego.go
+++ plugins/opa/pkg/rego/rego.go
@@ -3,6 +3,7 @@ package rego
 import (
 	"context"
 	"errors"
+	"fmt"
 	"strings"
 
 	"github.com/open-policy-agent/opa/ast"
@@ -10,6 +11,15 @@ import (
 	"github.com/open-policy-agent/opa/types"
 	"github.com/sirupsen/logrus"
 )
+
+// InsightsInfo exposes Insights perspective, for consideration in rego
+// policies.  FOr example, the context rego has been executed -
+// Continuous Integration, Admission Controller, or the Insights Agent.
+type InsightsInfo struct {
+	InsightsContext string
+	Cluster         string
+	Repository      string
+}
 
 type KubeDataFunction interface {
 	GetData(context.Context, string, string) ([]interface{}, error)
@@ -21,7 +31,7 @@ func (n NilDataFunction) GetData(ctx context.Context, group, kind string) ([]int
 	return nil, nil
 }
 
-func GetRegoQuery(body string, dataFn KubeDataFunction) *rego.Rego {
+func GetRegoQuery(body string, dataFn KubeDataFunction, insightsInfo *InsightsInfo) *rego.Rego {
 	return rego.New(
 		rego.Query("results = data"),
 		rego.Module("fairwinds", body),
@@ -30,14 +40,19 @@ func GetRegoQuery(body string, dataFn KubeDataFunction) *rego.Rego {
 				Name: "kubernetes",
 				Decl: types.NewFunction(types.Args(types.S, types.S), types.A),
 			},
-			getDataFunction(dataFn.GetData)))
+			getDataFunction(dataFn.GetData)),
+		rego.Function1(
+			&rego.Function{
+				Name: "insightsinfo",
+				Decl: types.NewFunction(types.Args(types.S), types.A),
+			},
+			GetInsightsInfoFunction(insightsInfo)))
 }
 
-func RunRegoForItem(ctx context.Context, regoStr string, params map[string]interface{}, obj map[string]interface{}, dataFn KubeDataFunction) ([]interface{}, error) {
-	r := GetRegoQuery(regoStr, dataFn)
+func RunRegoForItem(ctx context.Context, regoStr string, params map[string]interface{}, obj map[string]interface{}, dataFn KubeDataFunction, insightsInfo *InsightsInfo) ([]interface{}, error) {
+	r := GetRegoQuery(regoStr, dataFn, insightsInfo)
 	query, err := r.PrepareForEval(ctx)
 	if err != nil {
-		logrus.Errorf("Error while preparing rego query for evaluation: %v", err)
 		return nil, err
 	}
 	if params == nil {
@@ -50,7 +65,6 @@ func RunRegoForItem(ctx context.Context, regoStr string, params map[string]inter
 	evaluatedInput := rego.EvalInput(obj)
 	rs, err := query.Eval(ctx, evaluatedInput)
 	if err != nil {
-		logrus.Errorf("Error while evaluation query: %v", err)
 		return nil, err
 	}
 	return getOutputArray(rs), nil
@@ -62,21 +76,45 @@ func getDataFunction(fn func(context.Context, string, string) ([]interface{}, er
 		kind, err2 := getStringFromAST(kindAST)
 
 		if err1 != nil || err2 != nil {
-			return nil, errors.New("the kubernetes function should be passed a group and kind as strings")
+			return nil, rego.NewHaltError(errors.New("the kubernetes function should be passed a group and kind as strings"))
 		}
 		logrus.Infof("Getting Kubernetes data for %s/%s", group, kind)
 		items, err := fn(rctx.Context, group, kind)
 		if err != nil {
-			logrus.Errorf("Error while getting data for %s/%s: %v", group, kind, err)
-			return nil, err
+			return nil, rego.NewHaltError(fmt.Errorf("Error while getting data for %s/%s: %v", group, kind, err))
 		}
 		itemValue, err := ast.InterfaceToValue(items)
 		if err != nil {
-			logrus.Errorf("Error while converting data for %s/%s: %v", group, kind, err)
-			return nil, err
+			return nil, rego.NewHaltError(fmt.Errorf("Error while converting data for %s/%s: %v", group, kind, err))
 		}
 
 		return ast.NewTerm(itemValue), nil
+	}
+}
+
+// GetInsightsInfoFunction returns a function that is called from a rego
+// policy, to provide Insights information to the policy depending on the
+// function parameter.
+func GetInsightsInfoFunction(insightsInfo *InsightsInfo) func(rego.BuiltinContext, *ast.Term) (*ast.Term, error) {
+	return func(bc rego.BuiltinContext, inf *ast.Term) (*ast.Term, error) {
+		reqInfo, err := getStringFromAST(inf)
+		if err != nil {
+			return nil, rego.NewHaltError(fmt.Errorf("unable to convert requested InsightsInfo to string: %w", err))
+		}
+		var retInfo string
+		switch strings.ToLower(reqInfo) {
+		case "context":
+			retInfo = insightsInfo.InsightsContext
+		case "cluster":
+			retInfo = insightsInfo.Cluster
+		default:
+			return nil, rego.NewHaltError(fmt.Errorf("cannot return unknown Insights Info %q", reqInfo))
+		}
+		retInfoAsValue, err := ast.InterfaceToValue(retInfo)
+		if err != nil {
+			return nil, rego.NewHaltError(fmt.Errorf("unable to convert information %q to ast value: %w", retInfo, err))
+		}
+		return ast.NewTerm(retInfoAsValue), nil
 	}
 }
 
