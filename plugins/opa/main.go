5842acd0c82985aceac89ef1baec82e6606de466
Ivan Fetch <ivan@fairwinds.com>
2022-02-14 11:46:12 -0500

    Add Cobra/Viper and initial command-line processing of `target resources` (Kinds for V2 checks)

--- plugins/opa/main.go
+++ plugins/opa/main.go
@@ -1,45 +1,11 @@
+/*
+Copyright © 2022 FairwindsOps Inc
+
+*/
 package main
 
-import (
-	"context"
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"os"
-
-	"github.com/sirupsen/logrus"
-
-	opa "github.com/fairwindsops/insights-plugins/opa/pkg/opa"
-)
-
-const outputFile = "/output/opa.json"
-
-// Output is the format for the output file
-type Output struct {
-	ActionItems []opa.ActionItem
-}
+import "github.com/fairwindsops/insights-plugins/opa/cmd"
 
 func main() {
-	logrus.Info("Starting OPA reporter")
-	ctx := context.Background()
-	actionItems, runError := opa.Run(ctx)
-	if actionItems != nil {
-		logrus.Infof("Finished processing OPA checks, found %d Action Items", len(actionItems))
-
-		output := Output{ActionItems: actionItems}
-		value, err := json.Marshal(output)
-		if err != nil {
-			panic(err)
-		}
-
-		err = ioutil.WriteFile(outputFile, value, 0644)
-		if err != nil {
-			panic(err)
-		}
-	}
-	if runError != nil {
-		logrus.Error("There were errors while processing OPA checks.")
-		fmt.Fprintln(os.Stderr, runError)
-		os.Exit(1)
-	}
+	cmd.Execute()
 }
