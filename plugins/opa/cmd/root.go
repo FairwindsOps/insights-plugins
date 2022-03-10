94b1bc0b2f074484e0114a267540afe419b7a281
Ivan Fetch <ivan@fairwinds.com>
2022-03-10 16:15:07 -0500

    Use version pkg in OPA

--- plugins/opa/cmd/root.go
+++ plugins/opa/cmd/root.go
@@ -16,6 +16,7 @@ import (
 	"github.com/spf13/viper"
 
 	opa "github.com/fairwindsops/insights-plugins/plugins/opa/pkg/opa"
+	opaversion "github.com/fairwindsops/insights-plugins/plugins/opa/version"
 )
 
 var cfgFile string
@@ -89,7 +90,7 @@ func startOPAReporter() {
 		logrus.Debugf("Debugging is enabled...")
 	}
 	opa.CLIKubeTargets = opa.ProcessCLIKubeResourceTargets(viper.GetStringSlice("target-resource"))
-	logrus.Info("Starting OPA reporter")
+	logrus.Infof("Starting OPA reporter %s", opaversion.String())
 	ctx := context.Background()
 	actionItems, runError := opa.Run(ctx)
 	if actionItems != nil {
