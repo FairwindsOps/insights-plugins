b7ea5c161efe2e84830fabf263dcc44e9dbfdaef
Robert Brennan <contact@rbren.io>
2022-03-11 19:28:09 +0000

    set up scan schedule

--- scripts/scan-all.sh
+++ scripts/scan-all.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 set -eo pipefail
 
+have_vulns=()
+
 for d in ./plugins/*/ ; do
     if [[ $d == "_template" ]]; then
       continue
@@ -13,6 +15,20 @@ for d in ./plugins/*/ ; do
     name="quay.io/$REPOSITORY_NAME:$version"
     echo "scanning $name"
     docker pull $name
-    trivy $name
+
+    set +e
+    trivy -i --exit-code 123 $name
+    set -e
+    if [[ $? -eq 123 ]]; then
+      have_vulns+=($name)
+    fi
 done
+
+if (( ${#have_vulns[@]} != 0 )); then
+    echo "The following images have vulnerabilities:"
+    for image in "${have_vulns[@]}"; do
+     echo $image
+    done
+    exit 1
+fi
 
