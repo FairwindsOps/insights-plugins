52397a5ea374fdff5714201dd9c9ec211fbc51ea
Robert Brennan <contact@rbren.io>
2022-03-11 20:03:36 +0000

    sudo

--- .circleci/scripts/install-trivy.sh
+++ .circleci/scripts/install-trivy.sh
@@ -3,5 +3,5 @@ set -eo pipefail
 
 curl -L https://github.com/aquasecurity/trivy/releases/download/v0.23.0/trivy_0.23.0_Linux-64bit.tar.gz > trivy.tar.gz
 tar -xvf trivy.tar.gz
-mv ./trivy /usr/local/bin/trivy
+sudo mv ./trivy /usr/local/bin/trivy
 rm trivy.tar.gz
