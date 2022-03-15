f74f9e22ad9c56778b7d5ba5c25051d02ca03975
Robert Brennan <contact@rbren.io>
2022-03-15 14:31:54 +0000

    slack default channel

--- .circleci/scripts/env.sh
+++ .circleci/scripts/env.sh
@@ -3,6 +3,8 @@ set -eo pipefail
 sudo apt-get update && sudo apt-get install apt-transport-https ca-certificates -y
 sudo update-ca-certificates
 
+echo 'export SLACK_DEFAULT_CHANNEL=CPYBX810T' >> ${BASH_ENV}
+
 echo 'export GO111MODULE=on' >> ${BASH_ENV}
 echo 'export CI_SHA1=$CIRCLE_SHA1' >> ${BASH_ENV}
 echo 'export CI_BRANCH=$(echo "${CIRCLE_BRANCH:0:26}" | sed 's/[^a-zA-Z0-9]/-/g' | sed 's/-\+$//')' >> ${BASH_ENV}
