16841f5c8f38eae637ccd49712e5d1244b8198de
Robert Brennan <contact@rbren.io>
2022-03-24 13:21:24 +0000

    better update-go script

--- scripts/update-go-mod-all.sh
+++ scripts/update-go-mod-all.sh
@@ -8,16 +8,8 @@ for d in ./plugins/*/ ; do
     name=$(echo $d | sed 's/\.\/plugins\///' | sed 's/\///')
     if test -f "go.mod"; then
         echo "updating..."
-        mv go.mod old-go.mod
-        rm -f go.mod
-        rm -f go.sum
-        go mod init github.com/fairwindsops/insights-plugins/plugins/$name
-        if cat old-go.mod | grep replace; then
-          cat old-go.mod | grep replace >> go.mod
-        fi
-        rm old-go.mod
-        go get -d ./...
-        go mod tidy
+        go get -u -d ./...
+        go mod tidy -compat=1.17
     fi
     cd ../..
 done
