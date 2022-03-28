c8ddb32619fde6d21ab62eeb83472459c210f8a5
Robert Brennan <contact@rbren.io>
2022-03-28 13:53:38 +0000

    update update script

--- scripts/update-go-mod-all.sh
+++ scripts/update-go-mod-all.sh
@@ -4,12 +4,16 @@ set -eo pipefail
 go clean -modcache
 for d in ./plugins/*/ ; do
     echo -e "\n\n\n\n$d"
+    if [[ $SKIP == *"$d"* ]]; then
+      echo "skipping!"
+    fi
     cd $d
     name=$(echo $d | sed 's/\.\/plugins\///' | sed 's/\///')
     if test -f "go.mod"; then
         echo "updating..."
         go get -u -d ./...
-        go mod tidy -compat=1.17
+        go mod tidy
+        go test ./...
     fi
     cd ../..
 done
