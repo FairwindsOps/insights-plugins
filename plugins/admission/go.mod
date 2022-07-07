99660d1a93c83d914c642008cdb573f71473cbd1
ivanfetch-fw <87394843+ivanfetch-fw@users.noreply.github.com>
2022-07-07 06:10:20 -0600

    FWI-2243 - Add tests for admission-controller + Pluto code (#665)
    * [admission] add tests for Pluto, update `ProcessPluto` to accept PlutoTargetVersions, improve `ParsePlutoTargetVersions` error messaging, fix `ProcessPluto` bug where deprecated|removed was not being set
    
    * BUmp version and ChangeLog

--- plugins/admission/go.mod
+++ plugins/admission/go.mod
@@ -9,13 +9,13 @@ replace github.com/fairwindsops/insights-plugins/plugins/opa => ../opa
 require (
 	// IMPORTANT: Please also update the const  constant in pkg/pluto/pluto.go
 	// when updating the below Pluto version.
-	github.com/fairwindsops/pluto/v5 v5.9.0
+	github.com/fairwindsops/pluto/v5 v5.9.1-0.20220629194848-a932cda953da
 	github.com/fairwindsops/polaris v0.0.0-20220512134546-92f0b6e551df
 	github.com/sirupsen/logrus v1.8.1
 	github.com/stretchr/testify v1.7.4
 	github.com/thoas/go-funk v0.9.2
 	k8s.io/apimachinery v0.24.2
-	sigs.k8s.io/controller-runtime v0.12.1
+	sigs.k8s.io/controller-runtime v0.12.2
 )
 
 require (
@@ -29,8 +29,8 @@ require (
 require (
 	cloud.google.com/go v0.99.0 // indirect
 	github.com/Azure/go-autorest v14.2.0+incompatible // indirect
-	github.com/Azure/go-autorest/autorest v0.11.24 // indirect
-	github.com/Azure/go-autorest/autorest/adal v0.9.18 // indirect
+	github.com/Azure/go-autorest/autorest v0.11.27 // indirect
+	github.com/Azure/go-autorest/autorest/adal v0.9.20 // indirect
 	github.com/Azure/go-autorest/autorest/date v0.3.0 // indirect
 	github.com/Azure/go-autorest/logger v0.2.1 // indirect
 	github.com/Azure/go-autorest/tracing v0.6.0 // indirect
@@ -54,7 +54,7 @@ require (
 	github.com/gobuffalo/packr/v2 v2.8.3 // indirect
 	github.com/gobwas/glob v0.2.3 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
-	github.com/golang-jwt/jwt/v4 v4.3.0 // indirect
+	github.com/golang-jwt/jwt/v4 v4.4.2 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/gnostic v0.6.9 // indirect
@@ -94,11 +94,11 @@ require (
 	github.com/xeipuuv/gojsonpointer v0.0.0-20190905194746-02993c407bfb // indirect
 	github.com/xeipuuv/gojsonreference v0.0.0-20180127040603-bd5ef7bd5415 // indirect
 	github.com/yashtewari/glob-intersection v0.1.0 // indirect
-	golang.org/x/crypto v0.0.0-20220411220226-7b82a4e95df4 // indirect
+	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d // indirect
 	golang.org/x/mod v0.6.0-dev.0.20220106191415-9b9b3d81d5e3 // indirect
-	golang.org/x/net v0.0.0-20220617184016-355a448f1bc9 // indirect
-	golang.org/x/oauth2 v0.0.0-20220608161450-d0670ef3b1eb // indirect
-	golang.org/x/sys v0.0.0-20220615213510-4f61da869c0c // indirect
+	golang.org/x/net v0.0.0-20220624214902-1bab6f366d9e // indirect
+	golang.org/x/oauth2 v0.0.0-20220628200809-02e64fa58f26 // indirect
+	golang.org/x/sys v0.0.0-20220627191245-f75cf1eec38b // indirect
 	golang.org/x/term v0.0.0-20220526004731-065cf7ba2467 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	golang.org/x/time v0.0.0-20220609170525-579cf78fd858 // indirect
@@ -110,8 +110,8 @@ require (
 	k8s.io/apiextensions-apiserver v0.24.2 // indirect
 	k8s.io/client-go v0.24.2 // indirect
 	k8s.io/component-base v0.24.2 // indirect
-	k8s.io/klog/v2 v2.60.1 // indirect
-	k8s.io/kube-openapi v0.0.0-20220614142933-1062c7ade5f8 // indirect
+	k8s.io/klog/v2 v2.70.0 // indirect
+	k8s.io/kube-openapi v0.0.0-20220627174259-011e075b9cb8 // indirect
 	k8s.io/utils v0.0.0-20220210201930-3a6ce19ff2f9 // indirect
 	sigs.k8s.io/json v0.0.0-20220525155127-227cbc7cc124 // indirect
 	sigs.k8s.io/structured-merge-diff/v4 v4.2.1 // indirect
--- plugins/admission/pkg/pluto/pluto_test.go
+++ plugins/admission/pkg/pluto/pluto_test.go
@@ -1,3 +1,98 @@
 package pluto
 
-// TODO add tests for Pluto
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+
+	"github.com/fairwindsops/pluto/v5/pkg/api"
+	"github.com/stretchr/testify/require"
+)
+
+// plutoReportItems represents a slice of the upstream Pluto api.Output
+// struct, used to unmarshal the Pluto JSON output format for examination within
+// tests.
+type plutoReportItems struct {
+	Items []api.Output
+}
+
+const ingressV1Beta1Manifest = `
+# THIs ingress is deprecated in Kube 1.21 and removed in 1.22.
+apiVersion: networking.k8s.io/v1beta1
+kind: Ingress
+metadata:
+  name: test
+spec:
+  rules:
+  - host: test.domain.com
+    http:
+      paths:
+      - path: /
+        backend:
+          serviceName: test
+          servicePort: 80
+`
+
+func TestParsePlutoTargetVersions(t *testing.T) {
+	t.Parallel()
+	testCases := []struct {
+		description string
+		input       string // targetVersions to be parsed
+		want        map[string]string
+		expectError bool
+	}{
+		{
+			description: "single component k8s=v1.21.0",
+			input:       "k8s=v1.21.0",
+			want:        map[string]string{"k8s": "v1.21.0"},
+		},
+		{
+			description: "invalid single component k8s=1.21.0 lacking the leading v",
+			input:       "k8s=1.21.0",
+			expectError: true,
+		},
+		{
+			description: "multiple components k8s=v1.23.0 cert-manager=v1.8.0 istio=v1.14.0",
+			input:       "k8s=v1.23.0,cert-manager=v1.8.0,istio=v1.14.0",
+			want:        map[string]string{"k8s": "v1.23.0", "cert-manager": "v1.8.0", "istio": "v1.14.0"},
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.description, func(t *testing.T) {
+			got, err := ParsePlutoTargetVersions(tc.input)
+			if tc.expectError {
+				require.Error(t, err)
+			}
+			if !tc.expectError {
+				require.NoError(t, err)
+			}
+			require.Equal(t, tc.want, got)
+		})
+	}
+}
+
+func TestDeprecatedIngressV1Beta1WithKube121(t *testing.T) {
+	t.Parallel()
+	input := []byte(ingressV1Beta1Manifest)
+	plutoReportData, err := ProcessPluto(input, map[string]string{"k8s": "v1.21.0"})
+	require.NoError(t, err)
+	var plutoReport plutoReportItems
+	err = json.Unmarshal(plutoReportData.Contents, &plutoReport)
+	require.NoError(t, err, fmt.Sprintf("unmarshaling pluto contents from json: %v", string(plutoReportData.Contents)))
+	require.Equal(t, 1, len(plutoReport.Items), fmt.Sprintf("The Pluto output should have returned 1 item for a deprecated V1Beta1 Ingress. The full pluto output is: %v", string(plutoReportData.Contents)))
+	require.Equal(t, true, plutoReport.Items[0].Deprecated, fmt.Sprintf("Pluto should have returned that this API is deprecated. The full pluto output is: %v", string(plutoReportData.Contents)))
+	require.Equal(t, false, plutoReport.Items[0].Removed, fmt.Sprintf("Pluto should have returned that this API is not removed. The full pluto output is: %v", string(plutoReportData.Contents)))
+}
+
+func TestDeprecatedAndRemovedIngressV1Beta1WithKube123(t *testing.T) {
+	t.Parallel()
+	input := []byte(ingressV1Beta1Manifest)
+	plutoReportData, err := ProcessPluto(input, map[string]string{"k8s": "v1.23.0"})
+	require.NoError(t, err)
+	var plutoReport plutoReportItems
+	err = json.Unmarshal(plutoReportData.Contents, &plutoReport)
+	require.NoError(t, err, fmt.Sprintf("unmarshaling pluto contents from json: %v", string(plutoReportData.Contents)))
+	require.Equal(t, 1, len(plutoReport.Items), fmt.Sprintf("The Pluto output should have returned 1 item for a deprecated V1Beta1 Ingress. The full pluto output is: %v", string(plutoReportData.Contents)))
+	require.Equal(t, true, plutoReport.Items[0].Deprecated, fmt.Sprintf("Pluto should have returned that this API is deprecated. The full pluto output is: %v", string(plutoReportData.Contents)))
+	require.Equal(t, true, plutoReport.Items[0].Removed, fmt.Sprintf("Pluto should have returned that this API is not removed. The full pluto output is: %v", string(plutoReportData.Contents)))
+}
