ad77903aa6068b36498e07be5398eb045b0956b2
Robert Brennan <contact@rbren.io>
2022-07-26 10:12:34 -0400

    go tidy

--- scripts/update-go-mod-all.sh
+++ scripts/update-go-mod-all.sh
@@ -14,7 +14,7 @@ for d in ./plugins/*/ ; do
         echo "updating..."
         if [[ -z $UPDATE_PKG ]]; then
           go get -u -d ./...
-        else
+        elif [[ $UPDATE_PKG != "none" ]]; then
           go get -u -d $UPDATE_PKG
         fi
         go mod tidy
