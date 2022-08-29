6a4dc9fd704397a82cece7a22d019e42f10c0350
Ivan Fetch <ivan@fairwinds.com>
2022-08-29 15:56:16 -0600

    Add TFSec report and terraformConfig models, run tfsec per TF path and aggregate results into a single report

--- plugins/ci/pkg/ci/terraform.go
+++ plugins/ci/pkg/ci/terraform.go
@@ -1,18 +1,72 @@
 package ci
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"os/exec"
 	"path/filepath"
+	"strings"
 
 	"github.com/fairwindsops/insights-plugins/plugins/ci/pkg/commands"
+	"github.com/fairwindsops/insights-plugins/plugins/ci/pkg/models"
+	"github.com/sirupsen/logrus"
 )
 
-func (ci *CIScan) ProcessTerraformPaths() error {
+func (ci *CIScan) ProcessTerraformPaths() (models.ReportInfo, error) {
+	logrus.Infof("processing %d Terraform paths", len(ci.config.Terraform.Paths))
+	var reportProperties models.TFSecReportProperties
 	for _, terraformPath := range ci.config.Terraform.Paths {
-		_, err := commands.ExecWithMessage(exec.Command("tfsec", filepath.Join(ci.repoBaseFolder, terraformPath)), "scanning Terraform in "+terraformPath)
+		results, err := ci.ProcessTerraformPath(filepath.Join(ci.repoBaseFolder, terraformPath))
 		if err != nil {
-			return err
+			return models.ReportInfo{}, err
+		}
+		if len(results) > 0 {
+			reportProperties.Items = append(reportProperties.Items, results...)
 		}
 	}
-	return nil
+	TFSecVersion, err := commands.Exec("tfsec", "-v")
+	if err != nil {
+		return models.ReportInfo{}, fmt.Errorf("cannot get tfsec version: %w", err)
+	}
+	TFSecVersion = strings.TrimPrefix(TFSecVersion, "v")
+	report := models.ReportInfo{
+		Report:   "tfsec",
+		Version:  TFSecVersion,
+		Filename: "tfsec.json",
+	}
+	file, err := json.MarshalIndent(reportProperties, "", " ")
+	if err != nil {
+		return report, fmt.Errorf("while encoding report output: %w", err)
+	}
+	err = ioutil.WriteFile(report.Filename, file, 0644)
+	if err != nil {
+		return report, fmt.Errorf("while writing report output: %w", err)
+	}
+	return report, nil
+}
+
+func (ci *CIScan) ProcessTerraformPath(terraformPath string) ([]models.TFSecResult, error) {
+	logrus.Infof("processing terraform path %s", terraformPath)
+	terraformPathAsFileName := strings.ReplaceAll(strings.TrimPrefix(terraformPath, ci.repoBaseFolder), "/", "")
+	outputFile := filepath.Join(ci.config.Options.TempFolder, fmt.Sprintf("tfsec-output-%s", terraformPathAsFileName))
+	logrus.Infof("running tfsec and outputting to %s", outputFile)
+	_, err := commands.ExecWithMessage(exec.Command("tfsec", "-f", "json", "-O", outputFile, filepath.Join(ci.repoBaseFolder, terraformPath)), "scanning Terraform in "+terraformPath)
+	if err != nil {
+		return nil, err
+	}
+	var output models.TFSecReportProperties
+	data, err := ioutil.ReadFile(outputFile)
+	if err != nil {
+		logrus.Errorf("Error reading tfsec output from %s: %v", outputFile, err)
+		return nil, fmt.Errorf("while reading output from %s: %w", err)
+	}
+	err = json.Unmarshal(data, &output)
+	if err != nil {
+		logrus.Errorf("Error decoding tfsec output from %s: %v", outputFile, err)
+		return nil, fmt.Errorf("while decoding output from %s: %w", err)
+	}
+	logrus.Infof("%d tfsec results for path %s", len(output.Items), terraformPath)
+	logrus.Debugf("tfsec output for %s: %#v", terraformPath, output)
+	return output.Items, nil
 }
---
+++ plugins/ci/pkg/models/tfsec.go
@@ -0,0 +1,35 @@
+package models
+
+// TFSecResult contains a single TFSec finding.
+type TFSecResult struct {
+	Kind         string `json:"kind"`
+	ResourceName string `json:"resourceName"`
+	/*
+		ResourceNamespace string              `json:"resourceNamespace"`
+		ResourceContainer string              `json:"resourceContainer"`
+	*/
+	RuleID          string              `json:"rule_id"`
+	RuleDescription string              `json:"rule_description"`
+	Severity        string              `json:"severity"`
+	Description     string              `json:"description"`
+	Impact          string              `json:"impact"`
+	Links           []string            `json:"links"`
+	Resolution      string              `json:"resolution"`
+	Resource        string              `json:"resource"` // TF resource E.G. aws_instance.bastion
+	Location        TFSecResultLocation `json:"location"`
+}
+
+// TFSecResultLocation contains the range of line numbers that caused a TFSec
+// result.
+type TFSecResultLocation struct {
+	FileName  string `json:"filename"`
+	StartLine int64  `json:"start_line"`
+	EndLine   int64  `json:"end_line"`
+}
+
+// TFSecReportProperties contains TFSec output.
+type TFSecReportProperties struct {
+	Items []TFSecResult `json:"results"`
+}
+
+// TFSecReport is an Insights report.
