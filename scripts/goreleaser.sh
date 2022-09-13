50eb338a2b29dfe0d21a6f13622ea048d8607400
Ivan Fetch <ivan@fairwinds.com>
2022-09-13 13:35:03 -0600

    Update goreleaser.sh wrapper to only push some DOcker tags for the main branch, update workloads goreleaser config to test this branch-differentiation + only build the `FullCommit` docker image using the manifest to push other Docker tags

--- scripts/goreleaser.sh
+++ scripts/goreleaser.sh
@@ -11,6 +11,16 @@ echo "$(basename $0) creating local tag ${temporary_git_tag} for goreleaser"
 # The -f is included to overwrite existing tags, perhaps from previous CI jobs.
 git tag -f -m "temporary local tag for goreleaser" ${temporary_git_tag}
 export GORELEASER_CURRENT_TAG=${temporary_git_tag}
-goreleaser
+export skip_main_docker_tags=true
+if [ "${CIRCLE_BRANCH}" == "main" ] ; then
+  echo 'Setting skip_main_docker_tags to false because this is the main branch'
+export skip_main_docker_tags=false
+  fi
+hash envsubst
+cat .goreleaser.yml.envsubst |envsubst >.goreleaser.yml
+goreleaser $@
+if [ $? -eq 0 ] ; then
+  rm .goreleaser.yml # Keep git clean
+fi
 unset GORELEASER_CURRENT_TAG
 git tag -d ${temporary_git_tag}
