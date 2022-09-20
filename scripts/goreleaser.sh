d312d4faa0bf3fc8d180ca421b96296613927abe
Robert Brennan <contact@rbren.io>
2022-09-20 12:58:06 -0400

    formatting

--- scripts/goreleaser.sh
+++ scripts/goreleaser.sh
@@ -3,20 +3,21 @@
 # and creating a temporary git tag from an Insights plugin the version.txt file.
 
 function cleanup {
-if [ "${CIRCLE_TAG}" == "" ] ; then
-  echo "${this_script} deleting git tag ${temporary_git_tag} for goreleaser"
-  unset GORELEASER_CURRENT_TAG
-  git tag -d ${temporary_git_tag}
-fi
+  if [ "${CIRCLE_TAG}" == "" ] ; then
+    echo "${this_script} deleting git tag ${temporary_git_tag} for goreleaser"
+    unset GORELEASER_CURRENT_TAG
+    git tag -d ${temporary_git_tag}
+  fi
 }
-
 set -eE # errexit and errtrace
 trap 'cleanup' ERR
+
 this_script="$(basename $0)"
 if [ "${CIRCLE_BRANCH}" == "" ] ; then
   echo "${this_script} requires the CIRCLE_BRANCH environment variable, which is not set"
   exit 1
 fi
+
 hash envsubst
 hash goreleaser
 echo "${this_script} will run goreleaser for $(basename $(pwd))"
@@ -35,6 +36,7 @@ if [ "$(git config user.email)" == "" ] ; then
   export EMAIL='goreleaser_ci@fairwinds.com'
   echo "${this_script} using ${EMAIL} temporarily as the git user.email"
 fi
+
 temporary_git_tag=$(cat version.txt)
 echo "${this_script} creating git tag ${temporary_git_tag} for goreleaser"
 # The -f is included to overwrite existing tags, perhaps from previous CI jobs.
@@ -44,13 +46,14 @@ export skip_feature_docker_tags=false
 export skip_main_docker_tags=true
 if [ "${CIRCLE_BRANCH}" == "main" ] ; then
   echo "${this_script} setting skip_main_docker_tags to false, and skip_feature_docker_tags to true,  because this is the main branch"
-export skip_feature_docker_tags=true
-export skip_main_docker_tags=false
+  export skip_feature_docker_tags=true
+  export skip_main_docker_tags=false
 else
   # Use an adjusted git branch name as an additional docker tag, for feature branches.
   export feature_docker_tag=$(echo "${CIRCLE_BRANCH:0:26}" | sed 's/[^a-zA-Z0-9]/-/g' | sed 's/-\+$//')
   echo "${this_script} also using docker tag ${feature_docker_tag} since ${CIRCLE_BRANCH} is a feature branch"
 fi
+
 cat .goreleaser.yml.envsubst |envsubst >.goreleaser.yml
 goreleaser $@
 if [ $? -eq 0 ] ; then
