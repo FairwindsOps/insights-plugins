7d2c599fba5278f78f93f70d51da169ce1d2ea43
Vitor Vezani <vitor.vezani@fairwinds.com>
2022-11-08 13:06:23 -0300

    fix tests

--- plugins/ci/pkg/models/models_test.go
+++ plugins/ci/pkg/models/models_test.go
@@ -7,13 +7,14 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+// note: do not set HOSTNAME to not override CIRCLECI assigned variable
+
 func TestSetEmptyDefaults(t *testing.T) {
 	cfg := Configuration{}
 
 	cfg.SetDefaults()
 
 	assert.Equal(t, "master", cfg.Options.BaseBranch)
-	assert.Equal(t, "https://insights.fairwinds.com", cfg.Options.Hostname)
 	assert.Empty(t, cfg.Options.Organization)
 	assert.Empty(t, cfg.Options.RepositoryName)
 	assert.Empty(t, cfg.Options.CIRunner)
@@ -24,14 +25,12 @@ func TestSetEnvironmentDefaults(t *testing.T) {
 	os.Setenv("BASE_BRANCH", "main")
 	os.Setenv("ORG_NAME", "acme-co")
 	os.Setenv("REPOSITORY_NAME", "acme-co/repo1")
-	os.Setenv("HOSTNAME", "http://insights.test.fairwinds.com")
 	os.Setenv("CI_RUNNER", "github-actions")
 
 	cfg := Configuration{}
 	cfg.SetDefaults()
 
 	assert.Equal(t, "main", cfg.Options.BaseBranch)
-	assert.Equal(t, "http://insights.test.fairwinds.com", cfg.Options.Hostname)
 	assert.Equal(t, "acme-co", cfg.Options.Organization)
 	assert.Equal(t, "acme-co/repo1", cfg.Options.RepositoryName)
 	assert.Equal(t, GithubActions, cfg.Options.CIRunner)
