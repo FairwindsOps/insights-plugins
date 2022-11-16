830ff0e422e17f7a0e061d6475c5d5d77cc5f409
Vitor Rodrigo Vezani <vitor.vezani@fairwinds.com>
2022-11-16 15:18:49 -0300

    [3/5] FWI-3110 - do not fail on empty optional parameters (#719)
    * do not fail on optional parameters
    
    * update changelog and version
    
    * Update trivy to version 0.34.0 (#717)
    
    * bump version
    
    * add logGitCIRunnerHint function
    
    * add CI runner to info to insights headers
    
    * fix tests
    
    * add travis-ci
    
    * remove travis ci lol
    
    * travis fix
    
    * add fallback way to fetch master-hash
    
    * fix changelog
    
    * minor fixes
    
    * fix logGitCIRunnerHint fn
    
    * fix logging
    
    * add gitlab instructions
    
    * fix gitlab description
    
    * add travis description
    
    * fix message
    
    * fwi-266: Add the tfsec report to CI ci tf config (#716)
    
    * Add Terraform scanning
    
    * Add TFSec report and terraformConfig models, run tfsec per TF path and aggregate results into a single report
    
    * Fix Dockerfile to download tfsec after merging main
    
    * Replace deprecated `io/ioutil` packages, plus misc small adjustments to tfsec code
    
    * Fix complete tfsec.json report file path, individual tfsec scan directory represented as a single string, and add the -s flag to tfsec
    
    * Remove ResourceName and Kind from the tfsec report structure
    
    * Add .tf files to auto-scan
    
    * Update `TestAutoDetection` to include Terraform configuration
    
    * Add `testdata/repo1/terraform` for `TestAutoDetection`
    
    * Revert "fwi-266: Add the tfsec report to CI ci tf config" (#720)
    
    * Revert "fwi-266: Add the tfsec report to CI ci tf config (#716)"
    
    This reverts commit 04acab5f8ac4e28eb9d869255dff4052252488f6.
    
    * Update CHANGELOG.md
    
    * Update version.txt
    
    * FWI-3133 : Add tfsec report + fixes (#721)
    
    * Add Terraform scanning
    
    * Add TFSec report and terraformConfig models, run tfsec per TF path and aggregate results into a single report
    
    * Replace deprecated `io/ioutil` packages, plus misc small adjustments to tfsec code
    
    * Bump version
    
    * Add .tf files to auto-scan
    
    * Update `TestAutoDetection` to include Terraform configuration
    
    * Add `testdata/repo1/terraform` for `TestAutoDetection`
    
    * Only process `terraformPaths` if the tfsec report is enabled, auto-scan now enables the tfsec report if .tf files are found, and differentiate the types for terraform paths vs. terraform report config
    
    * FWI-3134: Update tfsec, pluto, and polaris used by CI to address additional `x/text` and `x/net` CVEs + bump Helm (#722)
    
    * bump version
    
    Co-authored-by: ivanfetch-fw <87394843+ivanfetch-fw@users.noreply.github.com>
    Co-authored-by: Robert Brennan <contact@rbren.io>

---
+++ plugins/ci/pkg/ci/ci_test.go
@@ -0,0 +1,39 @@
+package ci
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestReadConfigurationFromFile(t *testing.T) {
+	configFileContent := `
+options:
+  organization: fairwinds-production
+  baseBranch: master
+  repositoryName: FairwindsOps/fairwindsops-infrastructure
+  ciRunner: github-actions
+
+manifests:
+  yaml:
+  - ./inventory/production/clusters/prod/resources/manifests
+
+reports:
+  trivy:
+    enabled: false
+  opa:
+    enabled: false
+
+exemptions:
+  - image: 602401143452.dkr.ecr.us-west-2.amazonaws.com/amazon/aws-iam-authenticator:v0.5.2-scratch
+  - image: 602401143452.dkr.ecr.us-west-2.amazonaws.com/amazon/aws-iam-authenticator:v0.5.3-scratch
+`
+	reader := strings.NewReader(configFileContent)
+	cfg, err := readConfigurationFromReader(reader)
+	assert.NoError(t, err)
+	assert.Equal(t, "fairwinds-production", cfg.Options.Organization)
+	assert.Equal(t, "master", cfg.Options.BaseBranch)
+	assert.Equal(t, "FairwindsOps/fairwindsops-infrastructure", cfg.Options.RepositoryName)
+	assert.Empty(t, cfg.Options.CIRunner) // should not be read from file
+}
