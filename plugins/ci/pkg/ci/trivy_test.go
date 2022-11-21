7912791f4d0a34c8fd1703c1c908973d5b156782
Vitor Vezani <vitor.vezani@fairwinds.com>
2022-11-21 11:52:53 -0300

    use nginx instead of postgres

--- plugins/ci/pkg/ci/trivy_test.go
+++ plugins/ci/pkg/ci/trivy_test.go
@@ -7,16 +7,16 @@ import (
 )
 
 func TestWalkImages(t *testing.T) {
-	err := walkImages("testdata/images/postgres151bullseye.tar", func(filename, sha string, tags []string) {
-		assert.Equal(t, "postgres151bullseye.tar", filename)
-		assert.Equal(t, "sha256:5918a4f7e04aed7ac69d2b03b9b91345556db38709f9d6354056e3fdd9a8c02f", sha)
-		assert.Equal(t, []string{"postgres:15.1-bullseye"}, tags)
+	err := walkImages("testdata/images/nginx1232alpine.tar", func(filename, sha string, tags []string) {
+		assert.Equal(t, "nginx1232alpine.tar", filename)
+		assert.Equal(t, "sha256:2c8fe00b7b5c2a860377214dbf1c6b6bf7cee4d8d31df9672680a9a0186f2c22", sha)
+		assert.Equal(t, []string{"nginx:1.23.2-alpine"}, tags)
 	})
 	assert.NoError(t, err)
 }
 
 func TestClearString(t *testing.T) {
-	assert.Equal(t, "postgres151bullseye", clearString("postgres:15.1-bullseye"))
+	assert.Equal(t, "nginx1232alpine", clearString("nginx:1.23.2-alpine"))
 	assert.Equal(t, "fedorahttpdversion10", clearString("fedora/httpd:version1.0"))
 	assert.Equal(t, "myregistryhost5000fedorahttpdversion10", clearString("myregistryhost:5000/fedora/httpd:version1.0"))
 }
