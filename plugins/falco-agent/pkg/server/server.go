d2fbc2dbe1cce9c45459bef9da27519fca26bea0
Robert Brennan <contact@rbren.io>
2023-01-04 11:23:23 -0500

    refactor some more

--- plugins/falco-agent/pkg/server/server.go
+++ plugins/falco-agent/pkg/server/server.go
@@ -1,30 +1,21 @@
 package server
 
 import (
-	"context"
 	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
 
-	"github.com/fairwindsops/insights-plugins/plugins/falco-agent/pkg/kube"
 )
 
 func CreateServer(port int) (*http.Server, error) {
-	dynamic, restMapper, err := kube.GetKubeClient()
-	if err != nil {
-		return nil, err
-	}
-
 	r := mux.NewRouter()
 
 	r.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("OK"))
 	})
 
-	r.HandleFunc("/data", func(w http.ResponseWriter, r *http.Request) {
-		inputDataHandler(w, r, context.Background(), dynamic, restMapper)
-	}).Methods(http.MethodPost)
+	r.HandleFunc("/data", inputDataHandler).Methods(http.MethodPost)
 
 	r.HandleFunc("/output", outputDataHandler).Methods(http.MethodGet)
 
