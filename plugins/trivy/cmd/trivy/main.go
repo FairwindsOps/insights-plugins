906cb2e1db9b0dacae492c489135419f36ac88ff
Robert Brennan <contact@rbren.io>
2023-03-16 11:07:58 -0400

    add debug

--- plugins/trivy/cmd/trivy/main.go
+++ plugins/trivy/cmd/trivy/main.go
@@ -51,12 +51,16 @@ func main() {
 	for _, i := range imagesToScan {
 		logrus.Infof("%v - %v", i.Name, i.ID)
 	}
+	logrus.Infof("Latest report has %d images", len(lastReport.Images))
 	// Remove any images from the report that are no longer in the cluster
 	lastReport.Images = image.GetMatchingImages(lastReport.Images, images, false)
+	logrus.Infof("%d images after removing images no longer in cluster", len(lastReport.Images))
 	// Remove any images from the report that we're going to re-scan now
 	lastReport.Images = image.GetUnmatchingImages(lastReport.Images, imagesToScan, false)
+	logrus.Infof("%d images after removing images to be scanned", len(lastReport.Images))
 	// Remove any recommendations from the report that no longer have a corresponding image in the cluster
 	lastReport.Images = image.GetMatchingImages(lastReport.Images, images, true)
+	logrus.Infof("%d images after removing recommendations that don't match", len(lastReport.Images))
 
 	logrus.Infof("Starting image scans")
 	allReports := image.ScanImages(imagesToScan, maxConcurrentScans, extraFlags, false)
@@ -66,6 +70,7 @@ func main() {
 		recommendationsToScan := image.GetNewestVersionsToScan(ctx, allReports, imagesToScan)
 		// Remove any recommendations from the report that we're going to re-scan now
 		lastReport.Images = image.GetUnmatchingImages(lastReport.Images, recommendationsToScan, true)
+		logrus.Infof("%d images after removing recommendations that will be scanned", len(lastReport.Images))
 		logrus.Infof("Scanning %d recommended images", len(recommendationsToScan))
 		recommendationReport := image.ScanImages(recommendationsToScan, maxConcurrentScans, extraFlags, true)
 		logrus.Infof("Done scanning recommendations")
