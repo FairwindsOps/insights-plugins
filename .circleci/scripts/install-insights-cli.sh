6144901825b254a2ee3f21f6ba76b21dd8bea6f7
mhoss019 <31349386+mhoss019@users.noreply.github.com>
2023-03-24 08:20:18 -0400

    Update version of trivy and insights CLI for testing (#768)

--- .circleci/scripts/install-insights-cli.sh
+++ .circleci/scripts/install-insights-cli.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 set -eo pipefail
-insights_cli_version='1.0.2'
+insights_cli_version='1.0.4'
 
 echo "Installing insights-cli ${insights_cli_version}"
 curl -L https://github.com/FairwindsOps/insights-cli/releases/download/v${insights_cli_version}/insights-cli_${insights_cli_version}_linux_amd64.tar.gz > insights-cli.tar.gz
