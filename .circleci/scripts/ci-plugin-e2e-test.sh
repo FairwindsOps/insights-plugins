e952cc6f1f84562bfe699a080f9c785ae744f623
mhoss019 <31349386+mhoss019@users.noreply.github.com>
2023-03-29 14:27:38 -0400

    Add ci plugin e2e test (#771)

---
+++ .circleci/scripts/ci-plugin-e2e-test.sh
@@ -0,0 +1,48 @@
+#! /bin/bash
+set -eo pipefail
+
+if [[ ! " ${CHANGED[*]} " =~ " ci " ]]; then
+    echo "The CI plugin was not changed. Exiting"
+    exit 0
+fi
+
+DEMO_SERVER="https://stable-main.k8s.insights.fairwinds.com"
+curl $DEMO_SERVER/v0/ping
+
+THISDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+BASEDIR="$THISDIR/../.."
+TESTDIR="$BASEDIR/test/ci-plugin-e2e"
+CONFIG_FILE="$TESTDIR/fairwinds-insights.yaml"
+ci_script="$TESTDIR/insights-ci.sh"
+
+# Setup the $TESTDIR directory as a git repository
+cd $TESTDIR
+git config --global user.name "Insights CI"
+git config --global user.email insights@fairwinds.com
+git init
+git add .
+git commit -m "initial commit" || echo "Nothing new to commit"
+git remote add origin https://github.com/acme-co/cicd-test || echo "Remote origin already added"
+
+# Replace the required variables in the fairwinds-insights.yaml file
+repoName=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 16)
+sed -i "s/^  repositoryName: .*$/  repositoryName: ${repoName}/" $CONFIG_FILE
+
+# Run the insights-ci script and check the Action Items
+echo "FAIRWINDS_TOKEN=thisisacitoken" >> $BASH_ENV # FIXME: Not sure why we need to override the token this way...
+echo "Running CI/CD on sample repo"
+echo "The fairwinds-insights.yaml contents:"
+cat $CONFIG_FILE
+
+$ci_script &> output.txt || failed=false
+if [[ -n $failed ]]; then
+  cat output.txt
+  echo "CI script returned non-zero. Exiting."
+  exit 1
+fi
+
+echo -e "\n\nCI/CD is done. Validating output..."
+cat output.txt
+set +x
+cat output.txt | grep "22 new Action Items"
+cat output.txt | grep "0 fixed Action Items"
