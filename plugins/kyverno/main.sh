fcf2bbbe74696cfced784288e916aff1a921d7d4
John Slivka <johns@fairwinds.com>
2023-07-17 10:27:44 -0500

    FWI-4477 - Create Kyverno plugin for insights (#798)
    * Add Kyverno insights reporter plugin
    
    * simplify namespace iteration in kyverno bash script
    
    * modify script to only include latest policyreport, add schema with conformance test
    
    * update plugins
    
    * add kyverno values for e2e plugin test
    
    ---------
    
    Co-authored-by: Robert Brennan <contact@rbren.io>

---
+++ plugins/kyverno/main.sh
@@ -0,0 +1,41 @@
+#! /bin/bash
+set -e
+trap 'echo "Error on Line: $LINENO"' ERR
+echo "Starting kyverno"
+results_file=/output/kyverno.json
+
+json='{"policyReports":[], "clusterPolicyReports":[]}'
+
+# collect policyreports
+KIND="policyreport"
+echo "Retrieving namespaces"
+namespaces=$(kubectl get namespaces -o name | sed 's/namespace\///g')
+IFS=$'\n' namespaces=($namespaces)
+for namespace in "${namespaces[@]}"; do
+  reports=$(kubectl get $KIND -n $namespace -o name | sed "s/$KIND\.wgpolicyk8s\.io\///g")
+  IFS=$'\n' reports=($reports)
+
+  count=${#reports[@]}
+  echo "found $count $KIND for namespace $namespace"
+
+  for report in "${reports[@]}"; do
+    # to avoid overhead, collect only the last (most recent result) from the policyreport
+    report_json=$(kubectl get $KIND $report -n $namespace -o json | jq '.results |= [.[-1]]')
+    json="$(jq --argjson report_json "$report_json" '.policyReports += [$report_json]' <<< "$json")"
+  done
+done
+
+# collect clusterpolicyreports
+KIND="clusterpolicyreport"
+reports=$(kubectl get $KIND -n $namespace -o name | sed "s/$KIND\.wgpolicyk8s\.io\///g")
+IFS=$'\n' reports=($reports)
+
+count=${#reports[@]}
+echo "found $count $KIND for namespace $namespace"
+
+for report in "${reports[@]}"; do
+  report_json=$(kubectl get $KIND $report -n $namespace -o json | jq '.results |= [.[-1]]')
+  json="$(jq --argjson report_json "$report_json" '.clusterPolicyReports += [$report_json]' <<< "$json")"
+done
+
+echo $json | jq > $results_file
