4e57dcbf926b23a9aec4a828cdc14f6c673510f9
John Slivka <johns@fairwinds.com>
2023-08-16 11:13:40 -0500

    FWI-4713 - Avoid filename collisions in Falco scan for Insights (#810)
    * [falco-agent] allow aggregation to continue in the presence of malformed json, provided filenames which are guaranteed to be unique
    * [falco-agent] use afero filesystem wrapper, add unit tests

--- plugins/falco-agent/pkg/data/data.go
+++ plugins/falco-agent/pkg/data/data.go
@@ -3,26 +3,27 @@ package data
 import (
 	"context"
 	"encoding/json"
-	"io/ioutil"
+	"io/fs"
 	"os"
 	"path/filepath"
 	"time"
 
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/afero"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/client-go/dynamic"
 
-	"github.com/fairwindsops/controller-utils/pkg/log"
 	"k8s.io/apimachinery/pkg/api/meta"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 )
 
 func isLessThan24hrs(t time.Time) bool {
-	return time.Now().Sub(t) < 24*time.Hour
+	return time.Since(t) < 24*time.Hour
 }
 
-func deleteOlderFile(filePath string) (err error) {
-	err = os.Remove(filePath)
+func deleteOlderFile(fsWrapper afero.Fs, filePath string) (err error) {
+	err = fsWrapper.Remove(filePath)
 	if err != nil {
 		return
 
@@ -30,8 +31,8 @@ func deleteOlderFile(filePath string) (err error) {
 	return
 }
 
-func readDataFromFile(fileName string) (payload FalcoOutput, err error) {
-	data, err := os.ReadFile(fileName)
+func readDataFromFile(fsWrapper afero.Fs, fileName string) (payload FalcoOutput, err error) {
+	data, err := afero.ReadFile(fsWrapper, fileName)
 	if err != nil {
 		return
 	}
@@ -43,24 +44,32 @@ func readDataFromFile(fileName string) (payload FalcoOutput, err error) {
 }
 
 // Aggregate24hrsData return aggregated report for the past 24 hours
-func Aggregate24hrsData(dir string) (aggregatedData []FalcoOutput, err error) {
-	tmpfiles, err := ioutil.ReadDir(dir)
+func Aggregate24hrsData(fsWrapper afero.Fs, dir string) (aggregatedData []FalcoOutput, err error) {
+	tmpfiles, err := os.ReadDir(dir)
 	if err != nil {
 		return
 	}
 
+	var fileStat fs.FileInfo
+
 	for _, file := range tmpfiles {
-		if file.Mode().IsRegular() {
+		if file.Type().Perm().IsRegular() {
 			filename := filepath.Join(dir, file.Name())
-			if isLessThan24hrs(file.ModTime()) {
+			fileStat, err = fsWrapper.Stat(filename)
+			if err != nil {
+				return
+			}
+			if isLessThan24hrs(fileStat.ModTime()) {
 				var output FalcoOutput
-				output, err = readDataFromFile(filename)
+				output, err = readDataFromFile(fsWrapper, filename)
+				// skip malformed files and continue
 				if err != nil {
-					return
+					logrus.Warnf("Error reading file: %v", err)
+					continue
 				}
 				aggregatedData = append(aggregatedData, output)
 			} else {
-				err = deleteOlderFile(filename)
+				err = deleteOlderFile(fsWrapper, filename)
 				if err != nil {
 					return
 				}
@@ -75,7 +84,7 @@ func GetPodByPodName(ctx context.Context, dynamicClient dynamic.Interface, restM
 	fqKind := schema.FromAPIVersionAndKind("v1", "Pod")
 	mapping, err := restMapper.RESTMapping(fqKind.GroupKind(), fqKind.Version)
 	if err != nil {
-		log.GetLogger().Error(err, "Error retrieving mapping", "v1", "Pod")
+		logrus.Error(err, "Error retrieving mapping", "v1", "Pod")
 		return nil, err
 	}
 	pod, err := dynamicClient.Resource(mapping.Resource).Namespace(namespace).Get(ctx, podname, metav1.GetOptions{})
