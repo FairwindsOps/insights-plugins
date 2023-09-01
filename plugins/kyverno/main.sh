a48fd8f6e2ee716d299859201d479255e85e02da
John Slivka <johns@fairwinds.com>
2023-09-01 09:54:28 -0500

    [kyverno] FWI-4872 - Kyverno plugin script fails when `jq` argument list is more than `ARGMAX` (#813)
    * [kyverno] Modify script to use `jq` `--slurpfile` flag instead of a HERESTRING, to avoid "argument list too long" error
    * [kyverno] allow jq to skip missing results array in policyreports

--- plugins/kyverno/main.sh
+++ plugins/kyverno/main.sh
@@ -4,8 +4,9 @@ set -e
 trap 'echo "Error on Line: $LINENO"' ERR
 echo "Starting kyverno"
 results_file=/output/kyverno.json
-
-json='{"policyReports":[], "clusterPolicyReports":[]}'
+report_file=/tmp/report.json
+list_file=/tmp/results.json
+echo '{"policyReports":[], "clusterPolicyReports":[]}' > $list_file
 
 # check for policy and clusterpolicy CRDs
 kubectl get crd policies.kyverno.io >/dev/null || exit 1
@@ -31,8 +32,9 @@ for namespace in "${namespaces[@]}"; do
 
   for report in "${reports[@]}"; do
     # remove passed or skipped results to minimize payload and trigger a 'fixed' status for action items
-    report_json=$(kubectl -n $namespace get $KIND $report -o json | jq '. | del( ."results"[] | select(."result" == "pass" or ."result" == "skip") )')
-    policy_name=$(echo $report_json | jq -r '.results[0].policy')
+    report_json_full=$(kubectl -n $namespace get $KIND $report -o json)
+    echo $report_json_full | jq '. | del( ."results"[]? | select(."result" == "pass" or ."result" == "skip") )' > $report_file
+    policy_name=$(cat $report_file | jq -r '.results[0]?.policy')
 
     # use lookup table to minimize control plane load
     if [[ "$policy_name" != "null" && (! "${policies[*]}" =~ "${policy_name}") ]]; then
@@ -43,7 +45,7 @@ for namespace in "${namespaces[@]}"; do
       policy_title=$(kubectl -n $namespace get $policy_type $policy_name -o=jsonpath="{.metadata.annotations.policies\.kyverno\.io\/title}")
       policy_description=$(kubectl -n $namespace get $policy_type $policy_name -o=jsonpath="{.metadata.annotations.policies\.kyverno\.io\/description}")
       # populate the policy title and name
-      report_json="$(jq --arg title "$policy_title" --arg description "$policy_description" '. += {policyTitle: $title, policyDescription: $description}' <<< "$report_json")"
+      jq --arg title "$policy_title" --arg description "$policy_description" '. += {policyTitle: $title, policyDescription: $description}' < $report_file | sponge $report_file
 
       policies+=($policy_name)
       policy_titles["$policy_name"]=$policy_title
@@ -51,11 +53,11 @@ for namespace in "${namespaces[@]}"; do
     else
       policy_title=${policy_titles[$policy_name]}
       policy_description=${policy_descriptions[$policy_name]}
-      report_json="$(jq --arg title "$policy_title" --arg description "$policy_description" '. += {policyTitle: $title, policyDescription: $description}' <<< "$report_json")"
+      jq --arg title "$policy_title" --arg description "$policy_description" '. += {policyTitle: $title, policyDescription: $description}' < $report_file | sponge $report_file
     fi
 
     # append the modified policyreport to the output file
-    json="$(jq --argjson report_json "$report_json" '.policyReports += [$report_json]' <<< "$json")"
+    jq --slurpfile report_json "$report_file" '.policyReports += $report_json' < $list_file | sponge $list_file
   done
 done
 
@@ -69,8 +71,10 @@ count=${#cpol_reports[@]}
 echo "found $count $KIND"
 
 for report in "${cpol_reports[@]}"; do
-  report_json=$(kubectl get $KIND $report -o json | jq '. | del( ."results"[] | select(."result" == "pass" or ."result" == "skip") )')
-  policy_name=$(echo $report_json | jq -r '.results[0].policy')
+  # remove passed or skipped results to minimize payload and trigger a 'fixed' status for action items
+  report_json_full=$(kubectl -n $namespace get $KIND $report -o json)
+  echo $report_json_full | jq '. | del( ."results"[]? | select(."result" == "pass" or ."result" == "skip") )' > $report_file
+  policy_name=$(cat $report_file | jq -r '.results[0]?.policy')
 
   if [[ "$policy_name" != "null" && (! "${policies[*]}" =~ "${policy_name}") ]]; then
     policy_type="clusterpolicy"
@@ -80,17 +84,19 @@ for report in "${cpol_reports[@]}"; do
     policy_title=$(kubectl get $policy_type $policy_name -o=jsonpath="{.metadata.annotations.policies\.kyverno\.io\/title}")
     policy_description=$(kubectl get $policy_type $policy_name -o=jsonpath="{.metadata.annotations.policies\.kyverno\.io\/description}")
 
-    report_json="$(jq --arg title "$policy_title" --arg description "$policy_description" '. += {policyTitle: $title, policyDescription: $description}' <<< "$report_json")"
+    jq --arg title "$policy_title" --arg description "$policy_description" '. += {policyTitle: $title, policyDescription: $description}' < $report_file | sponge $report_file
 
     policy_titles["$policy_name"]=$policy_title
     policy_descriptions["$policy_name"]=$policy_description
   else
     policy_title=${policy_titles[$policy_name]}
     policy_description=${policy_descriptions[$policy_name]}
-    report_json="$(jq --arg title "$policy_title" --arg description "$policy_description" '. += {policyTitle: $title, policyDescription: $description}' <<< "$report_json")"
+    jq --arg title "$policy_title" --arg description "$policy_description" '. += {policyTitle: $title, policyDescription: $description}' < $report_file | sponge $report_file
   fi
 
-  json="$(jq --argjson report_json "$report_json" '.clusterPolicyReports += [$report_json]' <<< "$json")"
+  jq --slurpfile report_json "$report_file" '.clusterPolicyReports += $report_json' < $list_file | sponge $list_file
 done
 
-echo $json | jq > $results_file
+cat $list_file | jq > $results_file
+rm $report_file
+rm $list_file
