869a910129643fb7886ccc69c1bc9a9e19ebe5fb
Vitor Vezani <vitor.vezani@fairwinds.com>
2023-11-13 18:58:22 -0300

    updating pip

--- test/plugins-e2e/ci.sh
+++ test/plugins-e2e/ci.sh
@@ -4,8 +4,9 @@ echo "SETTING ENV"
 . /workspace/env.sh
 echo "ADDING PYTHON"
 apk add python3
-python3 --version
+echo "UPDATING PIP"
+pip3 install --upgrade pip
 echo "ADDING CHECK-JSONSCHEMA"
-pip3 install check-jsonschema==0.23.3
+pip3 install check-jsonschema
 echo "RUNNING TESTS"
 bash ./test/plugins-e2e/test.sh
