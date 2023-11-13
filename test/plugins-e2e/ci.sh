4b9151851d82453511ca92e5bcd2f059a67e727c
Vitor Vezani <vitor.vezani@fairwinds.com>
2023-11-13 18:37:59 -0300

    fix version of check-jsonschema

--- test/plugins-e2e/ci.sh
+++ test/plugins-e2e/ci.sh
@@ -4,7 +4,8 @@ echo "SETTING ENV"
 . /workspace/env.sh
 echo "ADDING PYTHON"
 apk add python3
+python3 --version
 echo "ADDING CHECK-JSONSCHEMA"
-pip3 install check-jsonschema
+pip3 install check-jsonschema==0.23.3
 echo "RUNNING TESTS"
 bash ./test/plugins-e2e/test.sh
