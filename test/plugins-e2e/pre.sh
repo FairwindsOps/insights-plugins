22e1616b0c089175b8897cbfe745ce0f31157c31
Robert Brennan <contact@rbren.io>
2023-11-13 13:41:17 -0500

    Update pre.sh

--- test/plugins-e2e/pre.sh
+++ test/plugins-e2e/pre.sh
@@ -1,5 +1,8 @@
 set -xeo pipefail
+echo "RUNNING PRE SCRIPT"
 mkdir output
 echo "export CI_BRANCH='$(echo "${CIRCLE_BRANCH:0:26}" | sed 's/[^a-zA-Z0-9]/-/g' | sed 's/-\+$//')'" >> env.sh
 ./.circleci/set_tags.sh
+echo "DONE SETTING TAGS"
 docker cp . e2e-command-runner:/workspace
+echo "DONE WITH DOCKER CP"
