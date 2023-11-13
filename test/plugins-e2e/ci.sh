78b2494a715cfa96b907c2a6038d7b121dba8908
Robert Brennan <contact@rbren.io>
2023-11-13 14:17:02 -0500

    Update ci.sh

--- test/plugins-e2e/ci.sh
+++ test/plugins-e2e/ci.sh
@@ -1,6 +1,10 @@
-set -eo pipefail
+set -xeo pipefail
 cd /workspace
+echo "SETTING ENV"
 . /workspace/env.sh
+echo "ADDING PYTHON"
 apk add python3
+echo "ADDING CHECK-JSONSCHEMA"
 pip3 install check-jsonschema
+echo "RUNNING TESTS"
 bash ./test/plugins-e2e/test.sh
