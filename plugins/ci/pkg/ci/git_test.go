94081001e857c48afc5162155830fc3a213353c3
Vitor Vezani <vitor.vezani@fairwinds.com>
2023-11-14 16:31:38 -0300

    add more tests

--- plugins/ci/pkg/ci/git_test.go
+++ plugins/ci/pkg/ci/git_test.go
@@ -11,6 +11,7 @@ import (
 )
 
 func TestExtractRepoNameFromOrigin(t *testing.T) {
+	assert.Equal(t, "FairwindsOps/insights-plugins", extractRepoNameFromOrigin("git@github.com:FairwindsOps/insights-plugins"))
 	assert.Equal(t, "FairwindsOps/insights-plugins", extractRepoNameFromOrigin("git@github.com:FairwindsOps/insights-plugins.git"))
 	assert.Equal(t, "FairwindsOps/insights-plugins", extractRepoNameFromOrigin("git@github.com:FairwindsOps/insights-plugins.git\n"))
 	assert.Equal(t, "", extractRepoNameFromOrigin(""))
