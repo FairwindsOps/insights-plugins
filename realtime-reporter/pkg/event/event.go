a61395a382b94ebc892034f6f4ee6360764bd245
John Slivka <johns@fairwinds.com>
2023-11-27 15:48:45 -0600

    tweak data model, emit empty data for delete events, pare down example resources to watch

--- realtime-reporter/pkg/event/event.go
+++ realtime-reporter/pkg/event/event.go
@@ -8,14 +8,14 @@ import (
 const eventVersion = 1
 
 type Event struct {
-	Version   int                    `json:"version"`
-	Timestamp string                 `json:"timestamp"`
-	Namespace string                 `json:"namespace"`
-	Workload  string                 `json:"workload"`
-	Data      map[string]interface{} `json:"data"`
+	Version   int            `json:"version"`
+	Timestamp string         `json:"timestamp"`
+	Namespace string         `json:"namespace"`
+	Workload  string         `json:"workload"`
+	Data      map[string]any `json:"data"`
 }
 
-func NewEvent(reportType string, namespace string, workload string, data map[string]interface{}) Event {
+func NewEvent(reportType string, namespace string, workload string, data map[string]any) Event {
 	timestamp := strconv.FormatInt(time.Now().UTC().UnixNano(), 10)
 
 	event := Event{
