d8a5f32eb7e9864faf4a572c6f5553a740dad06c
John Slivka <johns@fairwinds.com>
2023-11-28 15:45:38 -0600

    update event schema per discussion

--- realtime-reporter/pkg/event/event.go
+++ realtime-reporter/pkg/event/event.go
@@ -1,29 +1,30 @@
 package report
 
 import (
-	"strconv"
 	"time"
 )
 
 const eventVersion = 1
 
 type Event struct {
-	Version   int            `json:"version"`
-	Timestamp string         `json:"timestamp"`
-	Namespace string         `json:"namespace"`
-	Workload  string         `json:"workload"`
-	Data      map[string]any `json:"data"`
+	EventVersion int            `json:"event_version"`
+	Timestamp    int64          `json:"timestamp"`
+	Kind         string         `json:"kind"`
+	Namespace    string         `json:"namespace"`
+	Workload     string         `json:"workload"`
+	Data         map[string]any `json:"data"`
 }
 
-func NewEvent(reportType string, namespace string, workload string, data map[string]any) Event {
-	timestamp := strconv.FormatInt(time.Now().UTC().UnixNano(), 10)
+func NewEvent(kind string, namespace string, workload string, data map[string]any) Event {
+	timestamp := time.Now().UTC().UnixNano()
 
 	event := Event{
-		Version:   eventVersion,
-		Timestamp: timestamp,
-		Namespace: namespace,
-		Workload:  workload,
-		Data:      data,
+		EventVersion: eventVersion,
+		Kind:         kind,
+		Timestamp:    timestamp,
+		Namespace:    namespace,
+		Workload:     workload,
+		Data:         data,
 	}
 
 	return event
--- realtime-reporter/pkg/handlers/polaris.go
+++ realtime-reporter/pkg/handlers/polaris.go
@@ -29,7 +29,7 @@ func PolarisHandler(resourceType string) cache.ResourceEventHandlerFuncs {
 		}
 
 		reportMap := polarisReportInfoToMap(report)
-		e := evt.NewEvent("polaris", u.GetNamespace(), u.GetName(), reportMap)
+		e := evt.NewEvent(u.GetObjectKind().GroupVersionKind().Kind, u.GetNamespace(), u.GetName(), reportMap)
 		eventJson, err := json.Marshal(e)
 		if err != nil {
 			logrus.Errorf("Unable to marshal event: %v", err)
@@ -50,7 +50,7 @@ func PolarisHandler(resourceType string) cache.ResourceEventHandlerFuncs {
 		}
 
 		reportMap := polarisReportInfoToMap(report)
-		e := evt.NewEvent("polaris", u.GetNamespace(), u.GetName(), reportMap)
+		e := evt.NewEvent(u.GetObjectKind().GroupVersionKind().Kind, u.GetNamespace(), u.GetName(), reportMap)
 		eventJson, err := json.Marshal(e)
 		if err != nil {
 			logrus.Errorf("Unable to marshal event: %v", err)
@@ -61,7 +61,7 @@ func PolarisHandler(resourceType string) cache.ResourceEventHandlerFuncs {
 		// an event with empty data currently indicates the resource has been removed
 		u := obj.(*unstructured.Unstructured)
 
-		e := evt.NewEvent("polaris", u.GetNamespace(), u.GetName(), nil)
+		e := evt.NewEvent(u.GetObjectKind().GroupVersionKind().Kind, u.GetNamespace(), u.GetName(), nil)
 		eventJson, err := json.Marshal(e)
 		if err != nil {
 			logrus.Errorf("Unable to marshal event: %v", err)
