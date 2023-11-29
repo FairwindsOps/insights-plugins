65017e937ae08b75d7f4272bb16a4344854f4cae
jdesouza <james@fairwinds.com>
2023-11-29 20:49:42 -0300

    Fixed issue

--- plugins/cloud-costs/cloud-costs.sh
+++ plugins/cloud-costs/cloud-costs.sh
@@ -175,8 +175,10 @@ if [[ "$provider" == "gcp" ]]; then
 
   sql="SELECT main.* FROM \`$table\` AS main LEFT JOIN UNNEST(labels) as labels WHERE labels.key = '$tagkey' AND labels.value = '$tagvalue' and usage_start_time >= '$initial_date_time' AND usage_start_time < '$final_date_time' order by usage_start_time desc"
   bq --format=prettyjson query --max_rows=1000000 --nouse_legacy_sql "$sql" > /output/cloudcosts-tmp.json
+  echo "Google bigquey executing......"
+  bq wait
   echo "Google bigquey finished......"
-
+  
   mv /output/cloudcosts-tmp.json /output/cloudcosts.json
 
   echo "Saved GCP costs file in /output/cloudcosts.json"  
