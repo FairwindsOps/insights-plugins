6d5827ace1bdee86a4ad5c8b9e94f2db33517f7a
jdesouza <james@fairwinds.com>
2023-11-30 16:17:10 -0300

    GCP costs - removed bq messages (#846)
    * Fixed file name
    
    * Fixed file name
    
    * Revert "Fixed file name"
    
    This reverts commit dc9c1fbd7fff8d411b1d75dee6fdd052725a6d13.
    
    * Fixed file name
    
    * Debugging
    
    * Debugging
    
    * Debugging
    
    * Debugging
    
    * Debugging
    
    * Debugging
    
    * Fixed typo

--- plugins/cloud-costs/cloud-costs.sh
+++ plugins/cloud-costs/cloud-costs.sh
@@ -174,11 +174,13 @@ if [[ "$provider" == "gcp" ]]; then
   gcloud auth activate-service-account --key-file=/.config/gcp-key.json --project=$projectname
 
   sql="SELECT main.* FROM \`$table\` AS main LEFT JOIN UNNEST(labels) as labels WHERE labels.key = '$tagkey' AND labels.value = '$tagvalue' and usage_start_time >= '$initial_date_time' AND usage_start_time < '$final_date_time' order by usage_start_time desc"
-  bq --format=prettyjson query --max_rows=1000000 --nouse_legacy_sql "$sql" > /output/cloudcosts-tmp.json
+  bq --quiet --format=prettyjson query --max_rows=10000000 --nouse_legacy_sql "$sql" > /output/cloudcosts-tmp.json
   echo "Google bigquey finished......"
 
-  mv /output/cloudcosts-tmp.json /output/cloudcosts.json
+  awk '$0 == "[" {p=1} p' < /output/cloudcosts-tmp.json > /output/cloudcosts-tmp-clean.json
 
+  mv /output/cloudcosts-tmp-clean.json /output/cloudcosts.json
   echo "Saved GCP costs file in /output/cloudcosts.json"  
+
   exit 0
 fi
