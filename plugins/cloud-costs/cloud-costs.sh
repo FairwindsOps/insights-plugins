51597bda26890a9ed5430fb6eea32576afcbfd3a
jdesouza <james@fairwinds.com>
2023-12-08 17:45:39 -0300

    Removed gcloud auth (#851)

--- plugins/cloud-costs/cloud-costs.sh
+++ plugins/cloud-costs/cloud-costs.sh
@@ -171,8 +171,6 @@ if [[ "$provider" == "gcp" ]]; then
 
   echo "Google bigquey is running......"
 
-  gcloud auth activate-service-account --key-file=/.config/gcp-key.json --project=$projectname
-
   sql="SELECT main.* FROM \`$table\` AS main LEFT JOIN UNNEST(labels) as labels WHERE labels.key = '$tagkey' AND labels.value = '$tagvalue' and usage_start_time >= '$initial_date_time' AND usage_start_time < '$final_date_time' order by usage_start_time desc"
   bq --quiet --format=prettyjson query --max_rows=10000000 --nouse_legacy_sql "$sql" > /output/cloudcosts-tmp.json
   echo "Google bigquey finished......"
