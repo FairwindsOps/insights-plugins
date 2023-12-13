42683224f516c46642e6899bc71ec279654a1749
John Slivka <johns@fairwinds.com>
2023-12-13 16:29:06 -0600

    provisional commit for using retry-go library

--- realtime-reporter/go.mod
+++ realtime-reporter/go.mod
@@ -18,6 +18,7 @@ require (
 require (
 	github.com/OneOfOne/xxhash v1.2.8 // indirect
 	github.com/agnivade/levenshtein v1.1.1 // indirect
+	github.com/avast/retry-go v3.0.0+incompatible // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
--- realtime-reporter/pkg/client/client.go
+++ realtime-reporter/pkg/client/client.go
@@ -6,63 +6,62 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/avast/retry-go"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 )
 
 const (
-	maxTries = 3
+	maxTries  = 3
+	maxDelay  = 30 * time.Second
+	maxJitter = 10 * time.Millisecond
 )
 
+// TODO: this doesn't send a request to
+// apiURL = fmt.Sprintf("%s/v0/organizations/%s/clusters/%s/data/%s/incremental/failure", host, organization, cluster, reportType)
+// on failure, need to figure out how to do this with retry-go
 func UploadToInsights(timestamp int64, reportType string, payload []byte) error {
+	var resp *http.Response
 	host := viper.GetString("host")
 	organization := viper.GetString("organization")
 	cluster := viper.GetString("cluster")
 	token := viper.GetString("token")
 
-	var sendError bool
-	var tries int
+	err := retry.Do(
+		func() error {
+			var err error
+			apiURL := fmt.Sprintf("%s/v0/organizations/%s/clusters/%s/data/%s/incremental", host, organization, cluster, reportType)
 
-	for {
-		apiURL := fmt.Sprintf("%s/v0/organizations/%s/clusters/%s/data/%s/incremental", host, organization, cluster, reportType)
-		if sendError {
-			apiURL = fmt.Sprintf("%s/v0/organizations/%s/clusters/%s/data/%s/incremental/failure", host, organization, cluster, reportType)
-		}
-
-		req, err := http.NewRequest("POST", apiURL, bytes.NewBuffer(payload))
-		if err != nil {
-			return fmt.Errorf("error creating HTTP request: %v", err)
-		}
-
-		req.Header.Set("Content-Type", "application/json")
-		req.Header.Set("X-Fairwinds-Event-Timestamp", fmt.Sprintf("%d", timestamp))
-		req.Header.Set("Authorization", "Bearer "+token) // Add your authentication token if needed
-
-		// Create an HTTP client and send the request
-		resp, err := http.DefaultClient.Do(req)
-		if err != nil {
-			return fmt.Errorf("error making HTTP request: %v", err)
-		}
-		defer resp.Body.Close()
-
-		// Check the response
-		if resp.StatusCode == http.StatusOK {
-			break
-		} else {
-			logrus.Error("Failed to upload event - status code:", resp.StatusCode)
-
-			if sendError {
-				return fmt.Errorf("failed to upload event - status code: %d - tried %d times", resp.StatusCode, tries-1) // failed to upload error
+			req, err := http.NewRequest("POST", apiURL, bytes.NewBuffer(payload))
+			if err != nil {
+				return fmt.Errorf("error creating HTTP request: %v", err)
 			}
 
-			if tries >= maxTries {
-				time.Sleep(100 * time.Millisecond)
-				sendError = true
-			}
-			logrus.Warnf("failed to upload event - status code: %d - trying again...[%d/%d]", resp.StatusCode, tries, maxTries)
-			tries++
-		}
-	}
+			req.Header.Set("Content-Type", "application/json")
+			req.Header.Set("X-Fairwinds-Event-Timestamp", fmt.Sprintf("%d", timestamp))
+			req.Header.Set("Authorization", "Bearer "+token)
 
-	return nil
+			// create an HTTP client and send the request
+			resp, err = http.DefaultClient.Do(req)
+			if err != nil {
+				return err
+			}
+			defer resp.Body.Close()
+
+			return err
+		},
+		retry.Attempts(maxTries),
+		retry.OnRetry(func(n uint, err error) {
+			logrus.Infof("retrying request after error: %v", err)
+		}),
+		retry.DelayType(func(n uint, err error, config *retry.Config) time.Duration {
+			logrus.Infof("server fails with: %v", err.Error())
+			// apply a default exponential back off strategy
+			return retry.BackOffDelay(n, err, config)
+		}),
+		// apply random jitter
+		retry.MaxJitter(maxJitter),
+	)
+
+	return err
 }
