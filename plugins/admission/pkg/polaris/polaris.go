81e0de80120f897240345e6e292ac22a2dea7b71
jdesouza <james@fairwinds.com>
2024-08-26 08:21:32 -0300

    INSIGHTS-292 Upgrade pluto to 5.20.2

--- plugins/admission/pkg/polaris/polaris.go
+++ plugins/admission/pkg/polaris/polaris.go
@@ -43,12 +43,12 @@ func GetPolarisReport(ctx context.Context, config polarisconfiguration.Configura
 	return report, nil
 }
 
-func GetPolarisValidateResults(kind string, decoder *admission.Decoder, req admission.Request, config polarisconfiguration.Configuration) (*validator.Result, error) {
+func GetPolarisValidateResults(kind string, decoder admission.Decoder, req admission.Request, config polarisconfiguration.Configuration) (*validator.Result, error) {
 	var controller kube.GenericResource
 	var err error
 	if kind == "Pod" {
 		pod := corev1.Pod{}
-		err := (*decoder).Decode(req, &pod)
+		err := decoder.Decode(req, &pod)
 		if err != nil {
 			return nil, err
 		}
