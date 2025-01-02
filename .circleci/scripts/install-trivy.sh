34be02757ffbdf23070fc4559c3c49f3066b9f3b
jdesouza <james@fairwinds.com>
2025-01-02 10:06:13 -0300

    Fixing vulnerabilities

--- .circleci/scripts/install-trivy.sh
+++ .circleci/scripts/install-trivy.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 set -eo pipefail
 
-curl -L https://github.com/aquasecurity/trivy/releases/download/v0.58.0/trivy_0.58.0_Linux-64bit.tar.gz > trivy.tar.gz
+curl -L https://github.com/aquasecurity/trivy/releases/download/v0.58.1/trivy_0.58.1_Linux-64bit.tar.gz > trivy.tar.gz
 tar -xvf trivy.tar.gz
 sudo mv ./trivy /usr/local/bin/trivy
 rm trivy.tar.gz
