16bf70b8b3e0aef80f43add44f3873b21a233b60
Vitor Vezani <vitor.vezani@fairwinds.com>
2025-06-05 14:59:42 -0300

    fix tests

--- .circleci/scripts/ci-plugin-e2e-test.sh
+++ .circleci/scripts/ci-plugin-e2e-test.sh
@@ -44,5 +44,5 @@ fi
 echo -e "\n\nCI/CD is done. Validating output..."
 cat output.txt
 set +x
-cat output.txt | grep "35 new Action Items"
+cat output.txt | grep "33 new Action Items"
 cat output.txt | grep "0 fixed Action Items"
