d0f5288cd0fbebbf0fe48f829c89cabb73a517cb
jdesouza <james@fairwinds.com>
2025-11-26 17:04:20 -0300

    Fixing event poll interval

--- plugins/event-watcher/cmd/insights-event-watcher/commands/cloudwatch.go
+++ plugins/event-watcher/cmd/insights-event-watcher/commands/cloudwatch.go
@@ -101,7 +101,7 @@ func runCloudWatch(cmd *cobra.Command, args []string) error {
 		httpTimeoutSeconds,
 		rateLimitPerMinute,
 		consoleMode,
-		"", // eventPollInterval not used for CloudWatch
+		eventPollInterval,
 	)
 	if err != nil {
 		return fmt.Errorf("failed to create watcher: %w", err)
