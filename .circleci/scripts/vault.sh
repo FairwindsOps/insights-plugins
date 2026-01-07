d6fbab9d8eab4ad6b3a2ff054e1e011e2352d179
jdesouza <james@fairwinds.com>
2026-01-07 12:18:23 -0300

    Trying to fix

--- .circleci/scripts/vault.sh
+++ .circleci/scripts/vault.sh
@@ -1,26 +1,29 @@
 #! /bin/bash
-apt-get update
+
+if hash sudo; then
+  SUDO=sudo
+else
+  SUDO=""
+fi
+
+$SUDO apt-get update
 
 if ! hash yq; then
   curl -L "https://github.com/mikefarah/yq/releases/download/v4.30.6/yq_linux_amd64" > yq
   chmod +x yq
-  mv yq /usr/local/bin/
+  $SUDO mv yq /usr/local/bin/
 fi
 
 if ! hash curl; then
-  apt-get install -y curl
+  $SUDO apt-get install -y curl
 fi
 
 if ! hash unzip; then
-  apt-get install -y unzip
+  $SUDO apt-get install -y unzip
 fi
 
 cd /tmp
 curl -LO https://releases.hashicorp.com/vault/1.9.2/vault_1.9.2_linux_amd64.zip
 unzip vault_1.9.2_linux_amd64.zip
 
-if hash sudo; then
-  sudo mv vault /usr/local/bin/vault
-else
-  mv vault /usr/local/bin/vault
-fi
+$SUDO mv vault /usr/local/bin/vault
