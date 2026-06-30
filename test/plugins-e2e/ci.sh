5b67029ec348afb41f9378870c24c6327299f920
jdesouza <james@fairwinds.com>
2026-06-30 19:34:32 -0300

    INS-2496: [plugins] make End-To-End Test on Kubernetes step required for merge (#1243)
    * INS-2496: [plugins] make End-To-End Test on Kubernetes step required for merge
    
    * Fixes
    
    * Fixes
    
    * Fixes
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Bumped image
    
    * Empty
    
    * Fixed version
    
    * Fixed version
    
    * Empty

--- test/plugins-e2e/ci.sh
+++ test/plugins-e2e/ci.sh
@@ -2,11 +2,7 @@ set -xeo pipefail
 cd /workspace
 echo "SETTING ENV"
 . /workspace/env.sh
-echo "ADDING PYTHON"
-apk add python3
-echo "UPDATING PIP"
-pip3 install --upgrade pip
-echo "ADDING CHECK-JSONSCHEMA"
-pip3 install check-jsonschema
+echo "ADDING PYTHON AND CHECK-JSONSCHEMA"
+apk add --no-cache python3 check-jsonschema
 echo "RUNNING TESTS"
 bash ./test/plugins-e2e/test.sh
